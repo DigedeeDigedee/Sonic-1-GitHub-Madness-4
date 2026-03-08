f8c354b06a0b2a5116b9b8a43f4fb231e5ed8084
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-08 20:16:46 +0300

    fix credits I LOVE BEING USED TO OUTDATED STUFF

--- _maps/TryAgainFont.asm
+++ _maps/TryAgainFont.asm
@@ -1,36 +1,38 @@
-Map_ac5d:
-Map_ac5d_0: 	dc.w Map_ac5d_16-Map_ac5d
-Map_ac5d_2: 	dc.w Map_ac5d_1C-Map_ac5d
-Map_ac5d_4: 	dc.w Map_ac5d_22-Map_ac5d
-Map_ac5d_6: 	dc.w Map_ac5d_28-Map_ac5d
-Map_ac5d_8: 	dc.w Map_ac5d_2E-Map_ac5d
-Map_ac5d_A: 	dc.w Map_ac5d_34-Map_ac5d
-Map_ac5d_C: 	dc.w Map_ac5d_3A-Map_ac5d
-Map_ac5d_E: 	dc.w Map_ac5d_40-Map_ac5d
-Map_ac5d_10: 	dc.w Map_ac5d_46-Map_ac5d
-Map_ac5d_12: 	dc.w Map_ac5d_4C-Map_ac5d
-Map_ac5d_14: 	dc.w Map_ac5d_52-Map_ac5d
-Map_ac5d_16: 	dc.b $1
+Map_5d8a:
+Map_5d8a_0: 	dc.w Map_5d8a_18-Map_5d8a
+Map_5d8a_2: 	dc.w Map_5d8a_19-Map_5d8a
+Map_5d8a_4: 	dc.w Map_5d8a_1F-Map_5d8a
+Map_5d8a_6: 	dc.w Map_5d8a_25-Map_5d8a
+Map_5d8a_8: 	dc.w Map_5d8a_2B-Map_5d8a
+Map_5d8a_A: 	dc.w Map_5d8a_31-Map_5d8a
+Map_5d8a_C: 	dc.w Map_5d8a_37-Map_5d8a
+Map_5d8a_E: 	dc.w Map_5d8a_3D-Map_5d8a
+Map_5d8a_10: 	dc.w Map_5d8a_43-Map_5d8a
+Map_5d8a_12: 	dc.w Map_5d8a_49-Map_5d8a
+Map_5d8a_14: 	dc.w Map_5d8a_4F-Map_5d8a
+Map_5d8a_16: 	dc.w Map_5d8a_55-Map_5d8a
+Map_5d8a_18: 	dc.b $0
+Map_5d8a_19: 	dc.b $1
 	dc.b $F8, $5, $0, $2A, $F8
-Map_ac5d_1C: 	dc.b $1
+Map_5d8a_1F: 	dc.b $1
 	dc.b $F8, $5, $0, $26, $F8
-Map_ac5d_22: 	dc.b $1
+Map_5d8a_25: 	dc.b $1
 	dc.b $F8, $5, $0, $32, $F8
-Map_ac5d_28: 	dc.b $1
+Map_5d8a_2B: 	dc.b $1
 	dc.b $F8, $5, $0, $5C, $F8
-Map_ac5d_2E: 	dc.b $1
+Map_5d8a_31: 	dc.b $1
 	dc.b $F8, $5, $0, $1E, $F8
-Map_ac5d_34: 	dc.b $1
+Map_5d8a_37: 	dc.b $1
 	dc.b $F8, $1, $0, $46, $FC
-Map_ac5d_3A: 	dc.b $1
+Map_5d8a_3D: 	dc.b $1
 	dc.b $F8, $5, $0, $1A, $F8
-Map_ac5d_40: 	dc.b $1
+Map_5d8a_43: 	dc.b $1
 	dc.b $F8, $5, $0, $0, $F8
-Map_ac5d_46: 	dc.b $1
+Map_5d8a_49: 	dc.b $1
 	dc.b $F8, $5, $0, $42, $F8
-Map_ac5d_4C: 	dc.b $1
+Map_5d8a_4F: 	dc.b $1
 	dc.b $F8, $5, $0, $3E, $F8
-Map_ac5d_52: 	dc.b $2
+Map_5d8a_55: 	dc.b $2
 	dc.b $FA, $0, $0, $46, $FC
 	dc.b $FD, $0, $0, $47, $FC
 	even
