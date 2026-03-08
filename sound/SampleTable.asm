95f20fb627eadbfc77437bd96a52bbca171b5d45
DaxKatter2117 <daxternator2117@gmail.com>
2026-03-09 05:39:41 +0800

    DAMN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- sound/SampleTable.asm
+++ sound/SampleTable.asm
@@ -15,7 +15,8 @@ SampleTable:
 	dcSample	TYPE_PCM,	PokeTimpani,	15000			; $8B
 	dcSample	TYPE_PCM,	PokeTimpani,	11297			; $8C
 	dcSample	TYPE_PCM,	PokeTimpani,	7500			; $8D
-	dcSample	TYPE_PCM,	SegaPCM,	0, FLAGS_SFX		; $8E	NOTE: sample rate is auto-detected from WAV file
+	dcSample	TYPE_PCM,	DAMN,		16000, FLAGS_SFX	; $8E	NOTE: sample rate is auto-detected from WAV file
+	dcSample	TYPE_PCM,	SegaPCM,	0, FLAGS_SFX		; $8F	NOTE: sample rate is auto-detected from WAV file
 	dc.w	-1	; end marker
 
 ; ---------------------------------------------------------------
@@ -26,6 +27,7 @@ SampleTable:
 	incdac	PokeSnare, "sound/dac/DAX/PokeSnare.pcm"
 	incdac	PokeKickCrash, "sound/dac/DAX/PokeKickCrash.pcm"
 	incdac	PokeTimpani, "sound/dac/DAX/PokeTimpani.pcm"
+	incdac	DAMN, "sound/dac/DAX/DAMN.pcm"
 	incdac	SegaPCM, "sound/dac/sega.wav"
 
 	even
