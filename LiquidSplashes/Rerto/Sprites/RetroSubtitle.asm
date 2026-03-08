d5316fdd29c2357750289d840a85b445c83dde82
Malachi <malachi.ch1695@gmail.com>
2026-03-09 09:29:47 +1100

    Fix various issues with the sonic retro screen
    The reason the sprites weren't rendering is because they were using S3K sprite mappings. I also cleaned up the randomizer

--- LiquidSplashes/Rerto/Sprites/RetroSubtitle.asm
+++ LiquidSplashes/Rerto/Sprites/RetroSubtitle.asm
@@ -1,12 +1,12 @@
 .Map_RetroSubtitle:
 .Map_RetroSubtitle_0: 	dc.w .Map_RetroSubtitle_4-.Map_RetroSubtitle
 .Map_RetroSubtitle_2: 	dc.w .Map_RetroSubtitle_18-.Map_RetroSubtitle
-.Map_RetroSubtitle_4: 	dc.b $0, $3
-	dc.b $0, $E, $0, $C0, $0, $0
-	dc.b $0, $E, $0, $CC, $0, $20
-	dc.b $0, $E, $0, $D8, $0, $40
-.Map_RetroSubtitle_18: 	dc.b $0, $3
-	dc.b $0, $6, $0, $C0, $0, $0
-	dc.b $FF, $E, $0, $D5, $0, $1A
-	dc.b $FF, $1, $0, $E1, $0, $3A
+.Map_RetroSubtitle_4: 	dc.b $3
+	dc.b $00, $E, $0, $C0, $00
+	dc.b $00, $E, $0, $CC, $20
+	dc.b $00, $E, $0, $D8, $40
+.Map_RetroSubtitle_18: 	dc.b $3
+	dc.b $00, $6, $0, $C0, $00
+	dc.b $FF, $E, $0, $D5, $1A
+	dc.b $FF, $1, $0, $E1, $3A
 	even
