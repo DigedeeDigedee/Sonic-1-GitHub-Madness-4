7a870760d179af6e32f3c95c91348b067cf0b576
HS/C <carlos.edu.iagnecz@gmail.com>
2026-03-08 20:29:12 -0300

    Thanatos Credit loading + assets
    The mapping is the font, the text will be stored as ascii probably

--- hipncoolstuff/ThanatosCredits/Main.asm
+++ hipncoolstuff/ThanatosCredits/Main.asm
@@ -1,11 +1,12 @@
 
 	include "hipncoolstuff/ThanatosCredits/Variables.asm"
 
-	binclude "hipncoolstuff/ThanatosCredits/Credits Font.nem"
+NemThanatosCredits:	binclude "hipncoolstuff/ThanatosCredits/Credits Font.nem"
+EniThanatosCredits:	binclude "hipncoolstuff/ThanatosCredits/Credits Font.eni"
 
 VDP_ThanatosCredits:
 	dc.w $8700
-	dc.w $8C00
+	dc.w $8C01
 	dc.w $8B03
 	dc.w $9200
 	dc.w $9001
@@ -20,6 +21,7 @@ GM_ThanatosCredits:
 	jsr	(ClearPLC).l
 	jsr	(PaletteFadeOut).l
 
+	; Configure VDP
 	move.w	(v_vdp_buffer1).w,d0
 	ori.b	#$BF,d0
 	move.w	d0,(vdp_control_port).l
@@ -35,6 +37,7 @@ GM_ThanatosCredits:
 	ori.b	#$40,d0
 	move.w	d0,(vdp_control_port).l
 
+	; Empty object space
 	lea	(v_objspace).w,a1
 	moveq	#0,d0
 	move.w	#$7FF,d1
@@ -42,3 +45,35 @@ GM_ThanatosCredits:
 -	move.l	d0,(a1)+
 	dbf.w	d1,-
 
+	; Load palette
+	lea	(v_palette_fading),a1
+	move.w	#$20,d1
+
+-	move.l	PalThanatosCredits(pc,d1.w),(a1,d1.w)
+	subq.w	#4,d1
+	bpl.s	-
+
+	; moveq	#0,d0 ; d0 should be clear, if not use this
+	adda.w	#$20,a1
+	move.w	#$60/$4-1,d1
+-	move.l	d0,(a1)+
+	dbf.w	d1,-
+
+	; Load art/map
+	locVRAM	$20
+	lea	(NemThanatosCredits).l,a0 ; load art
+	jsr	(NemDec).l
+
+	lea	($FF0000).l,a1
+	lea	(EniThanatosCredits).l,a0 ; load map
+	move.w	#1,d0
+	jsr	(EniDec).l
+
+
+	jsr	(PaletteFadeIn).l
+
+	bra.s	*
+
+
+PalThanatosCredits:	binclude "hipncoolstuff/ThanatosCredits/Palette.pal"
+	rts
