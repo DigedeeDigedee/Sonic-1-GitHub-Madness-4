71785c05bbf07e6c06476ca72c1f53c012a48ff9
RoBEEFnik <epiccontent909@gmail.com>
2026-03-09 10:27:49 -0600

    pig wiener

--- _maps/Eggman - Scrap Brain 2.asm
+++ _maps/Eggman - Scrap Brain 2.asm
@@ -1,102 +1,97 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Eggman (SBZ2)
-; ---------------------------------------------------------------------------
-Map_SEgg_internal:	mappingsTable
-	mappingsTableEntry.w	.stand
-	mappingsTableEntry.w	.laugh1
-	mappingsTableEntry.w	.laugh2
-	mappingsTableEntry.w	.jump1
-	mappingsTableEntry.w	.jump2
-	mappingsTableEntry.w	.surprise
-	mappingsTableEntry.w	.starjump
-	mappingsTableEntry.w	.running1
-	mappingsTableEntry.w	.running2
-	mappingsTableEntry.w	.intube
-	mappingsTableEntry.w	.cockpit
+Map_a1f1: mappingsTable
+	mappingsTableEntry.w	Map_a1f1_0
+	mappingsTableEntry.w	Map_a1f1_1
+	mappingsTableEntry.w	Map_a1f1_2
+	mappingsTableEntry.w	Map_a1f1_3
+	mappingsTableEntry.w	Map_a1f1_4
+	mappingsTableEntry.w	Map_a1f1_5
+	mappingsTableEntry.w	Map_a1f1_6
+	mappingsTableEntry.w	Map_a1f1_7
+	mappingsTableEntry.w	Map_a1f1_8
+	mappingsTableEntry.w	Map_a1f1_9
+	mappingsTableEntry.w	Map_a1f1_10
 
-.stand:	spriteHeader
-	spritePiece	-$18, -4, 1, 1, $8F, 0, 0, 0, 0
-	spritePiece	-$10, -$18, 4, 3, 0, 0, 0, 0, 0
-	spritePiece	-$10, 0, 4, 4, $6F, 0, 0, 0, 0
-.stand_End
+Map_a1f1_0:	spriteHeader
+ spritePiece -$10, 0, 4, 4, $6F, 0, 0, 0, 0
+ spritePiece -$10, -$18, 3, 2, 0, 0, 0, 0, 0
+ spritePiece -$10, -8, 4, 1, 6, 0, 0, 0, 0
+Map_a1f1_0_End
 
-.laugh1:	spriteHeader
-	spritePiece	-$10, -$18, 4, 2, $E, 0, 0, 0, 0
-	spritePiece	-$10, -$18, 4, 3, 0, 0, 0, 0, 0
-	spritePiece	-$10, 0, 4, 4, $6F, 0, 0, 0, 0
-	spritePiece	-$18, -4, 1, 1, $8F, 0, 0, 0, 0
-.laugh1_End
-	even
-.laugh2:	spriteHeader
-	spritePiece	-$10, -$17, 4, 2, $E, 0, 0, 0, 0
-	spritePiece	-$10, -$17, 4, 3, 0, 0, 0, 0, 0
-	spritePiece	-$10, 1, 4, 4, $7F, 0, 0, 0, 0
-	spritePiece	-$18, -3, 1, 1, $8F, 0, 0, 0, 0
-.laugh2_End
-	even
-.jump1:	spriteHeader
-	spritePiece	-$10, -$C, 4, 4, $20, 1, 0, 0, 0
-	spritePiece	$10, -$B, 2, 1, $30, 1, 0, 0, 0
-	spritePiece	-$10, 8, 3, 2, $4E, 1, 0, 0, 0
-	spritePiece	-$10, -$14, 4, 3, 0, 0, 0, 0, 0
-.jump1_End
-	even
-.jump2:	spriteHeader
-	spritePiece	-$10, -$10, 4, 4, $20, 1, 0, 0, 0
-	spritePiece	$10, -$F, 2, 1, $30, 1, 0, 0, 0
-	spritePiece	-8, 8, 2, 3, $3E, 1, 0, 0, 0
-	spritePiece	-$10, -$18, 4, 3, 0, 0, 0, 0, 0
-.jump2_End
-	even
-.surprise:	spriteHeader
-	spritePiece	-$14, -$18, 4, 2, $16, 0, 0, 0, 0
-	spritePiece	$C, -$18, 1, 2, $1E, 0, 0, 0, 0
-	spritePiece	-$10, -$18, 4, 3, 0, 0, 0, 0, 0
-	spritePiece	-$10, 0, 4, 4, $6F, 0, 0, 0, 0
-.surprise_End
-	even
-.starjump:	spriteHeader
-	spritePiece	-$14, -$18, 4, 2, $16, 0, 0, 0, 0
-	spritePiece	$C, -$18, 1, 2, $1E, 0, 0, 0, 0
-	spritePiece	0, 4, 3, 2, $34, 1, 0, 0, 0
-	spritePiece	-$18, 4, 2, 2, $3A, 1, 0, 0, 0
-	spritePiece	-$10, -$10, 4, 4, $20, 1, 0, 0, 0
-	spritePiece	$10, -$F, 2, 1, $54, 1, 0, 0, 0
-	spritePiece	-$20, -$F, 2, 1, $54, 0, 0, 0, 0
-.starjump_End
+Map_a1f1_1:	spriteHeader
+ spritePiece -$10, -8, 4, 1, $A, 0, 0, 0, 0
+ spritePiece -$10, -$18, 3, 2, $E, 0, 0, 0, 0
+ spritePiece -$10, 0, 4, 4, $6F, 0, 0, 0, 0
+Map_a1f1_1_End
 
-.running1:	spriteHeader
-	spritePiece	-$10, -$10, 4, 4, $20, 1, 0, 0, 0
-	spritePiece	$10, -$F, 2, 1, $30, 1, 0, 0, 0
-	spritePiece	0, 4, 3, 2, $34, 1, 0, 0, 0
-	spritePiece	-$18, 4, 2, 2, $3A, 1, 0, 0, 0
-	spritePiece	-$10, -$18, 4, 3, 0, 0, 0, 0, 0
-.running1_End
+Map_a1f1_2:	spriteHeader
+ spritePiece -$10, 0, 4, 4, $7F, 0, 0, 0, 0
+ spritePiece -$10, -7, 4, 1, $A, 0, 0, 0, 0
+ spritePiece -$10, -$17, 3, 2, $E, 0, 0, 0, 0
+Map_a1f1_2_End
 
-.running2:	spriteHeader
-	spritePiece	-$10, -$12, 4, 4, $20, 1, 0, 0, 0
-	spritePiece	$10, -$11, 2, 1, $30, 1, 0, 0, 0
-	spritePiece	0, 9, 2, 2, $44, 1, 0, 0, 0
-	spritePiece	-8, 3, 1, 2, $48, 1, 0, 0, 0
-	spritePiece	-$18, $B, 2, 2, $4A, 1, 0, 0, 0
-	spritePiece	-$10, -$1A, 4, 3, 0, 0, 0, 0, 0
-.running2_End
-	even
-.intube:	spriteHeader
-	spritePiece	-$14, -$18, 4, 2, $16, 0, 0, 0, 0 ; Eggman inside tube in Final Zone
-	spritePiece	$C, -$18, 1, 2, $1E, 0, 0, 0, 0
-	spritePiece	-$10, -$18, 4, 3, 0, 0, 0, 0, 0
-	spritePiece	-$10, 0, 4, 4, $6F, 0, 0, 0, 0
-	spritePiece	-$10, -$20, 4, 2, $6F0, 1, 1, 1, 0
-	spritePiece	-$10, -$10, 4, 2, $6F0, 1, 1, 1, 0
-	spritePiece	-$10, 0, 4, 2, $6F0, 1, 1, 1, 0
-	spritePiece	-$10, $10, 4, 2, $6F0, 1, 1, 1, 0
-.intube_End
+Map_a1f1_3:	spriteHeader
+ spritePiece -$10, -$C, 4, 4, $20, 1, 0, 0, 0
+ spritePiece $10, -$B, 2, 1, $30, 1, 0, 0, 0
+ spritePiece -$10, 8, 3, 2, $4E, 1, 0, 0, 0
+ spritePiece -$10, -$14, 4, 3, 0, 0, 0, 0, 0
+Map_a1f1_3_End
 
-.cockpit:	spriteHeader
-	spritePiece	-$1C, -$14, 4, 2, $56, 0, 0, 0, 0 ; empty cockpit of Eggmobile in Final Zone
-	spritePiece	4, -$C, 3, 1, $5E, 0, 0, 0, 0
-	spritePiece	-4, -$14, 4, 2, $61, 0, 0, 0, 0
-.cockpit_End
+Map_a1f1_4:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $20, 1, 0, 0, 0
+ spritePiece $10, -$F, 2, 1, $30, 1, 0, 0, 0
+ spritePiece -8, 8, 2, 3, $3E, 1, 0, 0, 0
+ spritePiece -$10, -$18, 4, 3, 0, 0, 0, 0, 0
+Map_a1f1_4_End
+
+Map_a1f1_5:	spriteHeader
+ spritePiece -$14, -$18, 4, 2, $16, 0, 0, 0, 0
+ spritePiece $C, -$18, 1, 2, $1E, 0, 0, 0, 0
+ spritePiece -$10, -$18, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$10, 0, 4, 4, $6F, 0, 0, 0, 0
+Map_a1f1_5_End
+
+Map_a1f1_6:	spriteHeader
+ spritePiece -$14, -$18, 4, 2, $16, 0, 0, 0, 0
+ spritePiece $C, -$18, 1, 2, $1E, 0, 0, 0, 0
+ spritePiece 0, 4, 3, 2, $34, 1, 0, 0, 0
+ spritePiece -$18, 4, 2, 2, $3A, 1, 0, 0, 0
+ spritePiece -$10, -$10, 4, 4, $20, 1, 0, 0, 0
+ spritePiece $10, -$F, 2, 1, $54, 1, 0, 0, 0
+ spritePiece -$20, -$F, 2, 1, $54, 0, 0, 0, 0
+Map_a1f1_6_End
+
+Map_a1f1_7:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $20, 1, 0, 0, 0
+ spritePiece $10, -$F, 2, 1, $30, 1, 0, 0, 0
+ spritePiece 0, 4, 3, 2, $34, 1, 0, 0, 0
+ spritePiece -$18, 4, 2, 2, $3A, 1, 0, 0, 0
+ spritePiece -$10, -$18, 4, 3, 0, 0, 0, 0, 0
+Map_a1f1_7_End
+
+Map_a1f1_8:	spriteHeader
+ spritePiece -$10, -$12, 4, 4, $20, 1, 0, 0, 0
+ spritePiece $10, -$11, 2, 1, $30, 1, 0, 0, 0
+ spritePiece 0, 9, 2, 2, $44, 1, 0, 0, 0
+ spritePiece -8, 3, 1, 2, $48, 1, 0, 0, 0
+ spritePiece -$18, $B, 2, 2, $4A, 1, 0, 0, 0
+ spritePiece -$10, -$1A, 4, 3, 0, 0, 0, 0, 0
+Map_a1f1_8_End
+
+Map_a1f1_9:	spriteHeader
+ spritePiece -$14, -$18, 4, 2, $16, 0, 0, 0, 0
+ spritePiece $C, -$18, 1, 2, $1E, 0, 0, 0, 0
+ spritePiece -$10, -$18, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$10, 0, 4, 4, $6F, 0, 0, 0, 0
+ spritePiece -$10, -$20, 4, 2, $6F0, 1, 1, 1, 0
+ spritePiece -$10, -$10, 4, 2, $6F0, 1, 1, 1, 0
+ spritePiece -$10, 0, 4, 2, $6F0, 1, 1, 1, 0
+ spritePiece -$10, $10, 4, 2, $6F0, 1, 1, 1, 0
+Map_a1f1_9_End
+
+Map_a1f1_10:	spriteHeader
+ spritePiece -$1C, -$14, 4, 2, $56, 0, 0, 0, 0
+ spritePiece 4, -$C, 3, 1, $5E, 0, 0, 0, 0
+ spritePiece -4, -$14, 4, 2, $61, 0, 0, 0, 0
+Map_a1f1_10_End
 
 	even
--- _maps/Eggman.asm
+++ _maps/Eggman.asm
@@ -1,90 +1,92 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Eggman (boss levels)
-; ---------------------------------------------------------------------------
-Map_Eggman_internal:	mappingsTable
-	mappingsTableEntry.w	.ship
-	mappingsTableEntry.w	.facenormal1
-	mappingsTableEntry.w	.facenormal2
-	mappingsTableEntry.w	.facelaugh1
-	mappingsTableEntry.w	.facelaugh2
-	mappingsTableEntry.w	.facehit
-	mappingsTableEntry.w	.facepanic
-	mappingsTableEntry.w	.facedefeat
-	mappingsTableEntry.w	.flame1
-	mappingsTableEntry.w	.flame2
-	mappingsTableEntry.w	.blank
-	mappingsTableEntry.w	.escapeflame1
-	mappingsTableEntry.w	.escapeflame2
+Map_5cdd: mappingsTable
+	mappingsTableEntry.w	Map_5cdd_0
+	mappingsTableEntry.w	Map_5cdd_1
+	mappingsTableEntry.w	Map_5cdd_2
+	mappingsTableEntry.w	Map_5cdd_3
+	mappingsTableEntry.w	Map_5cdd_4
+	mappingsTableEntry.w	Map_5cdd_5
+	mappingsTableEntry.w	Map_5cdd_6
+	mappingsTableEntry.w	Map_5cdd_7
+	mappingsTableEntry.w	Map_5cdd_8
+	mappingsTableEntry.w	Map_5cdd_9
+	mappingsTableEntry.w	Map_5cdd_10
+	mappingsTableEntry.w	Map_5cdd_11
+	mappingsTableEntry.w	Map_5cdd_12
 
-.ship:	spriteHeader
-	spritePiece	-$1C, -$14, 1, 2, $A, 0, 0, 0, 0
-	spritePiece	$C, -$14, 2, 2, $C, 0, 0, 0, 0
-	spritePiece	-$1C, -4, 4, 3, $10, 0, 0, 1, 0
-	spritePiece	4, -4, 4, 3, $1C, 0, 0, 1, 0
-	spritePiece	-$14, $14, 4, 1, $28, 0, 0, 1, 0
-	spritePiece	$C, $14, 1, 1, $2C, 0, 0, 1, 0
-.ship_End
+Map_5cdd_0:	spriteHeader
+ spritePiece $C, -$14, 2, 2, $C, 0, 0, 0, 0
+ spritePiece -$1C, -4, 4, 3, $10, 0, 0, 1, 0
+ spritePiece 4, -4, 4, 3, $1C, 0, 0, 1, 0
+ spritePiece -$14, $14, 4, 1, $28, 0, 0, 1, 0
+ spritePiece $C, $14, 1, 1, $2C, 0, 0, 1, 0
+ spritePiece -$14, -$14, 1, 1, $35, 0, 0, 0, 0
+ spritePiece -$1C, -$C, 1, 1, $B, 0, 0, 0, 0
+ spritePiece -$14, -$C, 1, 1, 7, 0, 0, 0, 0
+ spritePiece -$1C, -$14, 1, 1, 3, 0, 0, 0, 0
+Map_5cdd_0_End
 
-.facenormal1:	spriteHeader
-	spritePiece	-$C, -$1C, 2, 1, 0, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 4, 2, 2, 0, 0, 0, 0
-.facenormal1_End
+Map_5cdd_1:	spriteHeader
+ spritePiece -$D, -$1C, 3, 1, 0, 0, 0, 0, 0
+ spritePiece -$D, -$14, 3, 1, 4, 0, 0, 0, 0
+ spritePiece -$D, -$C, 3, 1, 8, 0, 0, 0, 0
+Map_5cdd_1_End
 
-.facenormal2:	spriteHeader
-	spritePiece	-$C, -$1C, 2, 1, 0, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 4, 2, $35, 0, 0, 0, 0
-.facenormal2_End
+Map_5cdd_2:	spriteHeader
+ spritePiece -$D, -$1C, 3, 1, 0, 0, 0, 0, 0
+ spritePiece -$D, -$14, 3, 1, 4, 0, 0, 0, 0
+ spritePiece -$D, -$C, 3, 1, 8, 0, 0, 0, 0
+Map_5cdd_2_End
 
-.facelaugh1:	spriteHeader
-	spritePiece	-$C, -$1C, 3, 1, $3D, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 3, 2, $40, 0, 0, 0, 0
-	spritePiece	4, -$14, 2, 2, $46, 0, 0, 0, 0
-.facelaugh1_End
+Map_5cdd_3:	spriteHeader
+ spritePiece -$D, -$1C, 3, 3, $34, 0, 0, 0, 0
+Map_5cdd_3_End
 
-.facelaugh2:	spriteHeader
-	spritePiece	-$C, -$1C, 3, 1, $4A, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 3, 2, $4D, 0, 0, 0, 0
-	spritePiece	4, -$14, 2, 2, $53, 0, 0, 0, 0
-.facelaugh2_End
+Map_5cdd_4:	spriteHeader
+ spritePiece -$D, -$14, 3, 2, $3D, 0, 0, 0, 0
+ spritePiece -$D, -$1B, 1, 1, $34, 0, 0, 0, 0
+ spritePiece -5, -$1B, 1, 1, $37, 0, 0, 0, 0
+ spritePiece 3, -$1B, 1, 1, $3A, 0, 0, 0, 0
+Map_5cdd_4_End
 
-.facehit:	spriteHeader
-	spritePiece	-$C, -$1C, 3, 1, $57, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 3, 2, $5A, 0, 0, 0, 0
-	spritePiece	4, -$14, 2, 2, $60, 0, 0, 0, 0
-.facehit_End
+Map_5cdd_5:	spriteHeader
+ spritePiece -5, -$1F, 2, 4, $43, 0, 0, 0, 0
+ spritePiece -$C, -$1F, 1, 4, $47, 1, 0, 0, 0
+ spritePiece -$D, -$1C, 3, 3, $34, 0, 0, 0, 0
+Map_5cdd_5_End
 
-.facepanic:	spriteHeader
-	spritePiece	4, -$1C, 2, 1, $64, 0, 0, 0, 0
-	spritePiece	-$C, -$1C, 2, 1, 0, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 4, 2, $35, 0, 0, 0, 0
-.facepanic_End
+Map_5cdd_6:	spriteHeader
+ spritePiece -$D, -$1C, 3, 1, 0, 0, 0, 0, 0
+ spritePiece -$D, -$14, 3, 1, 4, 0, 0, 0, 0
+ spritePiece -$D, -$C, 3, 1, 8, 0, 0, 0, 0
+ spritePiece $A, -$20, 2, 1, $64, 0, 0, 0, 0
+Map_5cdd_6_End
 
-.facedefeat:	spriteHeader
-	spritePiece	-$C, -$1C, 3, 2, $66, 0, 0, 0, 0
-	spritePiece	-$C, -$1C, 3, 1, $57, 0, 0, 0, 0
-	spritePiece	-$14, -$14, 3, 2, $5A, 0, 0, 0, 0
-	spritePiece	4, -$14, 2, 2, $60, 0, 0, 0, 0
-.facedefeat_End
+Map_5cdd_7:	spriteHeader
+ spritePiece $22, 4, 2, 2, $2D, 0, 0, 0, 0
+Map_5cdd_7_End
 
-.flame1:	spriteHeader
-	spritePiece	$22, 4, 2, 2, $2D, 0, 0, 0, 0
-.flame1_End
+Map_5cdd_8:	spriteHeader
+ spritePiece -5, -$1F, 2, 4, $43, 0, 0, 0, 0
+ spritePiece -$C, -$1F, 1, 4, $47, 1, 0, 0, 0
+ spritePiece -$D, -$1C, 3, 3, $34, 0, 0, 0, 0
+Map_5cdd_8_End
 
-.flame2:	spriteHeader
-	spritePiece	$22, 4, 2, 2, $31, 0, 0, 0, 0
-.flame2_End
+Map_5cdd_9:	spriteHeader
+ spritePiece $22, 4, 1, 2, $31, 0, 0, 0, 0
+ spritePiece $2A, 4, 1, 1, $33, 0, 0, 0, 0
+Map_5cdd_9_End
 
-.blank:	spriteHeader
-.blank_End
+Map_5cdd_10:	spriteHeader
+Map_5cdd_10_End
 
-.escapeflame1:	spriteHeader
-	spritePiece	$22, 0, 3, 1, $12A, 0, 0, 0, 0
-	spritePiece	$22, 8, 3, 1, $12A, 0, 1, 0, 0
-.escapeflame1_End
+Map_5cdd_11:	spriteHeader
+ spritePiece $22, -8, 3, 4, $12D, 0, 0, 0, 0
+ spritePiece $3A, 0, 1, 2, $139, 0, 0, 0, 0
+Map_5cdd_11_End
 
-.escapeflame2:	spriteHeader
-	spritePiece	$22, -8, 3, 4, $12D, 0, 0, 0, 0
-	spritePiece	$3A, 0, 1, 2, $139, 0, 0, 0, 0
-.escapeflame2_End
+Map_5cdd_12:	spriteHeader
+ spritePiece $22, 0, 3, 1, $12A, 0, 0, 0, 0
+ spritePiece $22, 8, 3, 1, $12A, 0, 1, 0, 0
+Map_5cdd_12_End
 
 	even
