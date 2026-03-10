5953cb485c0de983b2a1b960ad6265379393364d
RoBEEFnik <epiccontent909@gmail.com>
2026-03-09 23:59:46 -0600

    shield

--- _maps/Shield and Invincibility.asm
+++ _maps/Shield and Invincibility.asm
@@ -1,63 +1,83 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - shield and invincibility stars
-; ---------------------------------------------------------------------------
-Map_Shield_internal:	mappingsTable
-	mappingsTableEntry.w	.shield1
-	mappingsTableEntry.w	.shield2
-	mappingsTableEntry.w	.shield3
-	mappingsTableEntry.w	.shield4
-	mappingsTableEntry.w	.stars1
-	mappingsTableEntry.w	.stars2
-	mappingsTableEntry.w	.stars3
-	mappingsTableEntry.w	.stars4
+Map_060c: mappingsTable
+	mappingsTableEntry.w	Map_060c_0
+	mappingsTableEntry.w	Map_060c_1
+	mappingsTableEntry.w	Map_060c_2
+	mappingsTableEntry.w	Map_060c_3
+	mappingsTableEntry.w	Map_060c_4
+	mappingsTableEntry.w	Map_060c_5
+	mappingsTableEntry.w	Map_060c_6
+	mappingsTableEntry.w	Map_060c_7
+	mappingsTableEntry.w	Map_060c_8
+	mappingsTableEntry.w	Map_060c_9
 
-.shield2:	spriteHeader
-	spritePiece	-$18, -$18, 3, 3, 0, 0, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, 9, 0, 0, 0, 0
-.shield1:spritePiece	-$18, 0, 3, 3, 0, 0, 1, 0, 0
-	spritePiece	0, 0, 3, 3, 9, 0, 1, 0, 0
-.shield2_End
+Map_060c_0:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, 0, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, 0, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, 0, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, 0, 1, 1, 0, 0
+Map_060c_0_End
 
-.shield3:	spriteHeader
-	spritePiece	-$17, -$18, 3, 3, $12, 1, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, $12, 0, 0, 0, 0
-	spritePiece	-$17, 0, 3, 3, $12, 1, 1, 0, 0
-	spritePiece	0, 0, 3, 3, $12, 0, 1, 0, 0
-.shield3_End
+Map_060c_1:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, 9, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $12, 0, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, 9, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $12, 0, 1, 0, 0
+Map_060c_1_End
 
-.shield4:	spriteHeader
-	spritePiece	-$18, -$18, 3, 3, 9, 1, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, 0, 1, 0, 0, 0
-	spritePiece	-$18, 0, 3, 3, 9, 1, 1, 0, 0
-	spritePiece	0, 0, 3, 3, 0, 1, 1, 0, 0
-.shield4_End
+Map_060c_2:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, $1B, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $24, 0, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, $1B, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $24, 0, 1, 0, 0
+Map_060c_2_End
 
-.stars1:	spriteHeader
-	spritePiece	-$18, -$18, 3, 3, 0, 0, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, 9, 0, 0, 0, 0
-	spritePiece	-$18, 0, 3, 3, 9, 1, 1, 0, 0
-	spritePiece	0, 0, 3, 3, 0, 1, 1, 0, 0
-.stars1_End
+Map_060c_3:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, $2D, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $2D, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, $2D, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $2D, 1, 1, 0, 0
+Map_060c_3_End
 
-.stars2:	spriteHeader
-	spritePiece	-$18, -$18, 3, 3, 9, 1, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, 0, 1, 0, 0, 0
-	spritePiece	-$18, 0, 3, 3, 0, 0, 1, 0, 0
-	spritePiece	0, 0, 3, 3, 9, 0, 1, 0, 0
-.stars2_End
+Map_060c_4:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, $36, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $3F, 0, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, $36, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $3F, 0, 1, 0, 0
+Map_060c_4_End
 
-.stars3:	spriteHeader
-	spritePiece	-$18, -$18, 3, 3, $12, 0, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, $1B, 0, 0, 0, 0
-	spritePiece	-$18, 0, 3, 3, $1B, 1, 1, 0, 0
-	spritePiece	0, 0, 3, 3, $12, 1, 1, 0, 0
-.stars3_End
+Map_060c_5:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, $48, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $51, 0, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, $48, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $51, 0, 1, 0, 0
+Map_060c_5_End
 
-.stars4:	spriteHeader
-	spritePiece	-$18, -$18, 3, 3, $1B, 1, 0, 0, 0
-	spritePiece	0, -$18, 3, 3, $12, 1, 0, 0, 0
-	spritePiece	-$18, 0, 3, 3, $12, 0, 1, 0, 0
-	spritePiece	0, 0, 3, 3, $1B, 0, 1, 0, 0
-.stars4_End
+Map_060c_6:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, 0, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, 9, 0, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, 9, 1, 1, 0, 0
+ spritePiece 0, 0, 3, 3, 0, 1, 1, 0, 0
+Map_060c_6_End
+
+Map_060c_7:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, 9, 1, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, 0, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, 0, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, 9, 0, 1, 0, 0
+Map_060c_7_End
+
+Map_060c_8:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, $12, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $1B, 0, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, $1B, 1, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $12, 1, 1, 0, 0
+Map_060c_8_End
+
+Map_060c_9:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, $1B, 1, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, $12, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, $12, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, $1B, 0, 1, 0, 0
+Map_060c_9_End
 
 	even
