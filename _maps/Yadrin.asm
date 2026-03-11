d86a77425e465d15f657bc32e61c3a0db830adce
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2026-03-11 15:06:55 -0300

    bad woody

--- _maps/Yadrin.asm
+++ _maps/Yadrin.asm
@@ -1,60 +1,48 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Yadrin enemy (SYZ)
-; ---------------------------------------------------------------------------
-Map_Yad_internal:	mappingsTable
-	mappingsTableEntry.w	.walk0
-	mappingsTableEntry.w	.walk1
-	mappingsTableEntry.w	.walk2
-	mappingsTableEntry.w	.walk3
-	mappingsTableEntry.w	.walk4
-	mappingsTableEntry.w	.walk5
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
 
-.walk0:	spriteHeader
-	spritePiece	-$C, -$C, 3, 1, 0, 0, 0, 0, 0
-	spritePiece	-$14, -4, 4, 3, 3, 0, 0, 0, 0
-	spritePiece	-4, -$14, 2, 1, $F, 0, 0, 0, 0
-	spritePiece	$C, -$C, 1, 3, $11, 0, 0, 0, 0
-	spritePiece	-4, 4, 3, 2, $31, 0, 0, 0, 0
-.walk0_End
-
-.walk1:	spriteHeader
-	spritePiece	-$C, -$C, 3, 1, $14, 0, 0, 0, 0
-	spritePiece	-$14, -4, 4, 3, $17, 0, 0, 0, 0
-	spritePiece	-4, -$14, 2, 1, $F, 0, 0, 0, 0
-	spritePiece	$C, -$C, 1, 3, $11, 0, 0, 0, 0
-	spritePiece	-4, 4, 3, 2, $31, 0, 0, 0, 0
-.walk1_End
-
-.walk2:	spriteHeader
-	spritePiece	-$C, -$C, 3, 2, $23, 0, 0, 0, 0
-	spritePiece	-$14, 4, 4, 2, $29, 0, 0, 0, 0
-	spritePiece	-4, -$14, 2, 1, $F, 0, 0, 0, 0
-	spritePiece	$C, -$C, 1, 3, $11, 0, 0, 0, 0
-	spritePiece	-4, 4, 3, 2, $31, 0, 0, 0, 0
-.walk2_End
-
-.walk3:	spriteHeader
-	spritePiece	-$C, -$C, 3, 1, 0, 0, 0, 0, 0
-	spritePiece	-$14, -4, 4, 3, 3, 0, 0, 0, 0
-	spritePiece	-4, -$14, 2, 1, $F, 0, 0, 0, 0
-	spritePiece	$C, -$C, 1, 3, $11, 0, 0, 0, 0
-	spritePiece	-4, 4, 3, 2, $37, 0, 0, 0, 0
-.walk3_End
-
-.walk4:	spriteHeader
-	spritePiece	-$C, -$C, 3, 1, $14, 0, 0, 0, 0
-	spritePiece	-$14, -4, 4, 3, $17, 0, 0, 0, 0
-	spritePiece	-4, -$14, 2, 1, $F, 0, 0, 0, 0
-	spritePiece	$C, -$C, 1, 3, $11, 0, 0, 0, 0
-	spritePiece	-4, 4, 3, 2, $37, 0, 0, 0, 0
-.walk4_End
-
-.walk5:	spriteHeader
-	spritePiece	-$C, -$C, 3, 2, $23, 0, 0, 0, 0
-	spritePiece	-$14, 4, 4, 2, $29, 0, 0, 0, 0
-	spritePiece	-4, -$14, 2, 1, $F, 0, 0, 0, 0
-	spritePiece	$C, -$C, 1, 3, $11, 0, 0, 0, 0
-	spritePiece	-4, 4, 3, 2, $37, 0, 0, 0, 0
-.walk5_End
-
-	even
+SME_k2Tod:	
+		dc.w SME_k2Tod_E-SME_k2Tod, SME_k2Tod_28-SME_k2Tod	
+		dc.w SME_k2Tod_47-SME_k2Tod, SME_k2Tod_61-SME_k2Tod	
+		dc.w SME_k2Tod_7B-SME_k2Tod, SME_k2Tod_95-SME_k2Tod	
+		dc.w SME_k2Tod_AF-SME_k2Tod	
+SME_k2Tod_E:	dc.b 5	
+		dc.b $C5, $F, 0, 0, $ED	
+		dc.b $E5, $F, 0, $10, $ED	
+		dc.b $C5, 7, 0, $20, $D	
+		dc.b $E5, 7, 0, $28, $D	
+		dc.b 5, $E, 0, $30, $ED	
+SME_k2Tod_28:	dc.b 6	
+		dc.b $C2, $F, 0, 0, $ED	
+		dc.b $E6, $F, 0, $10, $ED	
+		dc.b $C4, 7, 0, $20, $D	
+		dc.b $E4, 7, 0, $28, $D	
+		dc.b 6, $E, 0, $30, $ED	
+		dc.b $E1, $F, 0, $10, $ED	
+SME_k2Tod_47:	dc.b 5	
+		dc.b $BF, $F, 0, 0, $ED	
+		dc.b $DD, $F, 0, $10, $ED	
+		dc.b $BC, 7, 0, $20, $D	
+		dc.b $DC, 7, 0, $28, $B	
+		dc.b $FA, $E, 0, $30, $ED	
+SME_k2Tod_61:	dc.b 5	
+		dc.b $BE, $F, 0, 0, $ED	
+		dc.b $D9, $F, 0, $10, $ED	
+		dc.b $B9, 7, 0, $20, $D	
+		dc.b $D9, 7, 0, $28, $A	
+		dc.b $F6, $E, 0, $30, $ED	
+SME_k2Tod_7B:	dc.b 5	
+		dc.b $BE, $F, 0, 0, $ED	
+		dc.b $D7, $F, 0, $10, $ED	
+		dc.b $B7, 7, 0, $20, $D	
+		dc.b $D6, 7, 0, $28, 7	
+		dc.b $F0, $E, 0, $30, $ED	
+SME_k2Tod_95:	dc.b 5	
+		dc.b $BF, $F, 0, 0, $EC	
+		dc.b $D7, $F, 0, $10, $EB	
+		dc.b $B8, 7, 0, $20, $B	
+		dc.b $D8, 7, 0, $28, 7	
+		dc.b $F4, $E, 0, $30, $EB	
+SME_k2Tod_AF:	dc.b 0	
+		even
