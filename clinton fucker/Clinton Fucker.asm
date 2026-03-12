46cc860a2367810c446552ebca8f8eb3be83c506
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-03-11 19:23:15 -0500

    he kills you now

--- clinton fucker/Clinton Fucker.asm
+++ clinton fucker/Clinton Fucker.asm
@@ -12,6 +12,14 @@ clifuck.Speed   =	$36
 ;REND.BEHIND	= 6
 ;REND.TOGGLE	= 7
 
+;STAT.XDIR	= 0
+;STAT.YDIR	= 1
+;STAT.UNK2	= 2
+;STAT.LIFTING	= 3 
+;STAT.UNK4	= 4
+;STAT.PUSHED	= 5 
+;STAT.UNK6	= 6
+;STAT.KILLED	= 7
 
 ClintonFucker:
 	moveq	#0,d0
@@ -35,7 +43,7 @@ CliFucker_Wait:
 	addq.w	#1,clifuck.Timer(a0)
 	cmpi.w	#60*3,clifuck.Timer(a0)
 	beq.s	.SayHi
-	cmpi.w	#60*5,clifuck.Timer(a0)
+	cmpi.w	#60*6,clifuck.Timer(a0)
 	ble.s	.Exit
 	addq.b	#2,obRoutine(a0)
 
@@ -48,6 +56,9 @@ CliFucker_Wait:
 	jmp	MegaPCM_PlaySample
 
 CliFucker_Init2:
+	move.b	#$1E,d0			; placeholder
+	jsr	QueueSound2
+
 	tst.l	(v_plc_buffer).w
 	bne.s	.Exit
 	move.l	#Nem_Clinton,v_plc_buffer	; copy PLC to RAM directly
@@ -60,9 +71,10 @@ CliFucker_Init2:
 	move.b	#$14,obActWid(a0)
 	move.b	#$10,obHeight(a0)
 	move.b	#8,obWidth(a0)
+	;move.b	#$9A,obColType(a0)
 	move.b	#1,obFrame(a0)
 	move.w	#400,clifuck.Speed(a0)
-	move.w	#32,clifuck.Accel(a0)
+	move.w	#48,clifuck.Accel(a0)
 	move.w	#0,(v_limitleft1).w
 	move.w	#0,(v_limitleft2).w
 	st	v_clintonfucker
@@ -85,9 +97,22 @@ CliFucker_Main:
 	move.w	clifuck.Accel(a0), d1
 	jsr	ChaseObject
 	jsr	SpeedToPos
+
+	move.w	#$13,d1
+	move.w	#$28,d2
+	jsr	Obj44_SolidWall
+	move.b	obStatus(a0),d0
+	andi.b	#%101000,d0
+	beq.s	.NoKill
+	move.l	a0,-(sp)
+	lea	v_player,a0
+	jsr	KillSonic
+	move.l	(sp)+,a0
+.NoKill
 	lea	Ani_Clinton,a1
 	jsr	AnimateSprite
 	jsr	DisplaySprite
+
 	rts
 
 
