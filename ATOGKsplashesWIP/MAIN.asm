cd466154a9a4eb3f1fac471000b9c14dda040d57
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2026-03-12 18:16:39 -0300

    test

--- ATOGKsplashesWIP/MAIN.asm
+++ ATOGKsplashesWIP/MAIN.asm
@@ -14,7 +14,7 @@
 ; ===========================================================================
 
 MultiSplash_Init:
-		bsr.w	PaletteFadeOut		
+		bsr.w	Set_Fadeout		
 		move    #$2700,sr           ; Goodbye inters!
 		lea     (VDPCtrl).l,a6
 		move.w  #$8004,(a6)          
@@ -27,87 +27,113 @@ MultiSplash_Init:
 		move.w  d0,(VDPCtrl).l
 
 MarioTeam_Screen:
-		bsr.w   ClearScreen
+		bsr.w   Set_ClearScreen
 		move.l  #$40000000,($C00004).l      ; Art
 		lea     (Nem_MarioTeam).l,a0  
-		bsr.w   NemDec
+		bsr.w   Set_NemDec
 		
 		lea     (Chunk).l,a1
 		lea     (Eni_MarioTeam).l,a0  
-		bsr.w   VDP_Location
-		bsr.w   TilemapToVRAM
+		bsr.w   Set_VDPLoc
+		bsr.w   Set_TilemapToVRAM
 		
 		moveq   #palid_Sonic,d0      
-		bsr.w   PalLoad2
+		bsr.w   Set_PalLoad2
 		
 		move.w  #$100,(Timer).w          ; Time     
 		move.w  (VDP_buff).w,d0
 		ori.b   #$40,d0
 		move.w  d0,(VDPCtrl).l
-		bsr.w	PaletteFadeIn
+		bsr.w	Set_Fadein
 		
 
 MarioTeam_Wait:
-		bsr.w	PaletteFadeOut		
+		bsr.w	Set_Fadeout		
 		bsr.w   LoopDelay
 		bne.s   MarioTeam_Wait
 
 ; ---------------------------------------------------------------------------
 
 RickTeamheads_Screen:		
-		bsr.w   ClearScreen
+		bsr.w   Set_ClearScreen
 		move.l  #$40000000,($C00004).l      ; Art
 		lea     (Nem_RickTeam).l,a0   
-		bsr.w   NemDec
+		bsr.w   Set_NemDec
 		
 		lea     (Chunk).l,a1
 		lea     (Eni_RickTeam).l,a0   
-		bsr.w   VDP_Location
-		bsr.w   TilemapToVRAM
+		bsr.w   Set_VDPLoc
+		bsr.w   Set_TilemapToVRAM
 		
 		moveq   #palid_Savethemoonsonic,d0      
-		bsr.w   PalLoad2
+		bsr.w   Set_PalLoad2
 		
 		move.w  #$100,(Timer).w          ; Time
 		move.w  (VDP_buff).w,d0
 		ori.b   #$40,d0
 		move.w  d0,(VDPCtrl).l
-		bsr.w	PaletteFadeIn
+		bsr.w	Set_Fadein
 
 RickTeam_Wait:
-		bsr.w	PaletteFadeOut
+		bsr.w	Set_Fadeout
 		bsr.w   LoopDelay
 		bne.s   RickTeam_Wait
 
 ; ---------------------------------------------------------------------------
 
 TeamTesticle_Screen:		
-		bsr.w   ClearScreen
+		bsr.w   Set_ClearScreen
 		move.l  #$40000000,($C00004).l      ; Art
 		lea     (Nem_Testicle).l,a0  
-		bsr.w   NemDec
+		bsr.w   Set_NemDec
 		
 		lea     (Chunk).l,a1
 		lea     (Eni_Testicle).l,a0  
-		bsr.w   VDP_Location
-		bsr.w   TilemapToVRAM
+		bsr.w   Set_VDPLoc
+		bsr.w   Set_TilemapToVRAM
 		
 		moveq   #palid_S2,d0      
-		bsr.w   PalLoad2		
+		bsr.w   Set_PalLoad2		
 		move.w  #$100,(Timer).w          ; Time
 		move.w  (VDP_buff).w,d0
 		ori.b   #$40,d0
 		move.w  d0,(VDPCtrl).l
-		bsr.w	PaletteFadeIn
+		bsr.w	Set_Fadein
 
 Testicle_Wait:
-		bsr.w	PaletteFadeOut		
+		bsr.w	Set_Fadeout		
 		bsr.w   LoopDelay
 		bne.s   Testicle_Wait
 
 Exit_Splashes:
-		; Next slash 
+		jmp	RunSplashes	
+; ===========================================================================
+; ---------------------------------------------------------------------------
+; MISC
+; ---------------------------------------------------------------------------
+; ===========================================================================
 
+Set_VDPLoc:
+		jmp   VDP_Location
+		rts
+Set_TilemapToVRAM:		
+		jmp  TilemapToVRAM
+		rts
+Set_Fadein:		
+		jmp PaletteFadeIn		
+        rts
+Set_Fadeout:		
+		jmp PaletteFadeOut		
+        rts	
+Set_ClearScreen:
+        jmp ClearScreen
+        rts	
+Set_NemDec:
+        jmp NemDec
+        rts		
+Set_PalLoad2:
+        jmp  PalLoad2
+        rts			
 ; ===========================================================================
 ; ---------------------------------------------------------------------------
 ; ART, MAPS AND PALETTE
@@ -115,10 +141,18 @@ Exit_Splashes:
 ; ===========================================================================
 		
 Nem_MarioTeam:  incbin  "Art/marioteam.nem"
+                even
 Eni_MarioTeam:  incbin  "Eni/marioteam.eni"
+                even
 Nem_RickTeam:   incbin  "Art/rick teamheads.nem"
+                even
 Eni_RickTeam:   incbin  "Eni/rick teamheads.eni"
+                even
 Nem_Testicle:   incbin  "Art/TEAM TESTICLE.nem"
-Eni_Testicle:   incbin  "Eni/TEAM TESTICLE.eni"		
-Pal_S2:   incbin  "sonic2main.bin"	  
+                even
+Eni_Testicle:   incbin  "Eni/TEAM TESTICLE.eni"	
+                even	
+Pal_S2:         incbin  "sonic2main.bin"	 
+                even 
 Pal_STMsonic:   incbin  "STMsonic.bin"	  
+                even
