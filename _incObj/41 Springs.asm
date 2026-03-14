5a44788b00fd7f1acfebc802fcdbd642808a1f33
dawido90183 <92670419+dawido90183@users.noreply.github.com>
2026-03-14 18:42:29 +0100

    lovely new spring sound

--- _incObj/41 Springs.asm
+++ _incObj/41 Springs.asm
@@ -84,8 +84,8 @@ Spring_BounceUp:
 		move.b	#2,obRoutine(a1)
 		bclr	#3,obStatus(a0)
 		clr.b	obSolid(a0)
-		move.w	#sfx_Spring,d0
-		jsr	(QueueSound2).l	; play spring sound
+		move.b	#$C7, d0
+		jsr		(MegaPCM_PlaySample).l
 
 Spring_AniUp:	; Routine 4
 		lea	(Ani_Spring).l,a1
