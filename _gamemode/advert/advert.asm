946f8888ebe804ba9f1f9d3fa569447ea0730d85
nanothehedghog <thenewuser0202@gmail.com>
2026-03-16 22:30:26 +0100

    This rom hack is sponsored by RAID SHADOW LEGENDS and NO modding roms

--- _gamemode/advert/advert.asm
+++ _gamemode/advert/advert.asm
@@ -140,7 +140,9 @@ GM_Advert:
 		advertdata 30,10,Ad_Carbuncle.art,Ad_Carbuncle.fg,Ad_Carbuncle.pal,bgm_Sunset,0
 		advertdata 30,5,Ad_Support.art,Ad_Support.fg,Ad_Support.pal,bgm_ChickenDance,0
 		advertdata 12,5,Ad_Tonic.art,Ad_Tonic.fg,Ad_Tonic.pal,bgm_Elevator,0
-		advertdata 30,5,Ad_SonicUnderground.art,Ad_SonicUnderground.fg,Ad_SonicUnderground.pal,bgm_Continue,0		
+		advertdata 30,5,Ad_SonicUnderground.art,Ad_SonicUnderground.fg,Ad_SonicUnderground.pal,bgm_Continue,0
+		advertdata 30,5,Ad_RaidShadowLegends.art,Ad_RaidShadowLegends.fg,Ad_RaidShadowLegends.pal,bgm_Easton,0
+		advertdata 30,10,Ad_TamperThingy.art,Ad_TamperThingy.fg,Ad_TamperThingy.pal,bgm_Basillica,0
 .tablee:
 .eyecatch1:	advertdata 5,10,Ad_Eyecatch.art,Ad_Eyecatch.fg1,Ad_Eyecatch.pal,0,dRightBack
 .eyecatch2:	advertdata 5,10,Ad_Eyecatch.art,Ad_Eyecatch.fg2,Ad_Eyecatch.pal,bgm_EuroSega,0
@@ -202,7 +204,16 @@ Ad_Support:
 .fg:		binclude "_gamemode/advert/Map - Support.eni"
 .art:		binclude "_gamemode/advert/Art - Support.nem"
 		even
-
+Ad_RaidShadowLegends:
+.pal:		binclude "_gamemode/advert/ad-raidshadowlegends-pal.unc"
+.fg:		binclude "_gamemode/advert/ad-raidshadowlegends-map.eni"
+.art:		binclude "_gamemode/advert/ad-raidshadowlegends-art.nem"
+		even
+Ad_TamperThingy:
+.pal:		binclude "_gamemode/advert/ad-TamperThingy-pal.unc"
+.fg:		binclude "_gamemode/advert/ad-TamperThingy-map.eni"
+.art:		binclude "_gamemode/advert/ad-TamperThingy-art.nem"
+		even
 Ad_Tonic:
 .pal:		binclude "_gamemode/advert/Tonic Pal.bin"
 .fg:		binclude "_gamemode/advert/Tonic Map.eni"
