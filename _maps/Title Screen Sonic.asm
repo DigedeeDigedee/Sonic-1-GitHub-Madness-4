4c38333f3048cc5151a81e10cb6697d61ea0da1e
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-17 15:07:30 +0300

    give tonic more animation

--- _maps/Title Screen Sonic.asm
+++ _maps/Title Screen Sonic.asm
@@ -1,10 +1,12 @@
-Map_7e36: mappingsTable
-	mappingsTableEntry.w	Map_7e36_0
-	mappingsTableEntry.w	Map_7e36_1
-	mappingsTableEntry.w	Map_7e36_2
-	mappingsTableEntry.w	Map_7e36_3
+Map_a7be: mappingsTable
+	mappingsTableEntry.w	Map_a7be_0
+	mappingsTableEntry.w	Map_a7be_1
+	mappingsTableEntry.w	Map_a7be_2
+	mappingsTableEntry.w	Map_a7be_3
+	mappingsTableEntry.w	Map_a7be_4
+	mappingsTableEntry.w	Map_a7be_5
 
-Map_7e36_0:	spriteHeader
+Map_a7be_0:	spriteHeader
  spritePiece -$23, -$20, 4, 4, 0, 0, 0, 0, 0
  spritePiece -3, -$20, 4, 4, $10, 0, 0, 0, 0
  spritePiece $1D, -$20, 2, 4, $20, 0, 0, 0, 0
@@ -13,9 +15,23 @@ Map_7e36_0:	spriteHeader
  spritePiece $D, 0, 4, 4, $48, 0, 0, 0, 0
  spritePiece $2D, -$20, 1, 2, $58, 0, 0, 0, 0
  spritePiece $2D, 8, 1, 2, $5A, 0, 0, 0, 0
-Map_7e36_0_End
+Map_a7be_0_End
 
-Map_7e36_1:	spriteHeader
+Map_a7be_1:	spriteHeader
+ spritePiece -$23, -8, 2, 1, $5C, 0, 0, 0, 0
+ spritePiece -$33, 0, 3, 4, $5E, 0, 0, 0, 0
+ spritePiece -$1B, 0, 1, 2, $6A, 0, 0, 0, 0
+ spritePiece -$23, -$20, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -3, -$20, 4, 4, $10, 0, 0, 0, 0
+ spritePiece $1D, -$20, 2, 4, $20, 0, 0, 0, 0
+ spritePiece -$33, 0, 4, 4, $28, 0, 0, 0, 0
+ spritePiece -$13, 0, 4, 4, $38, 0, 0, 0, 0
+ spritePiece $D, 0, 4, 4, $48, 0, 0, 0, 0
+ spritePiece $2D, -$20, 1, 2, $58, 0, 0, 0, 0
+ spritePiece $2D, 8, 1, 2, $5A, 0, 0, 0, 0
+Map_a7be_1_End
+
+Map_a7be_2:	spriteHeader
  spritePiece -$32, -$19, 2, 1, $5C, 0, 0, 0, 0
  spritePiece -$42, -$11, 4, 4, $5E, 0, 0, 0, 0
  spritePiece -$22, 7, 2, 2, $6E, 0, 0, 0, 0
@@ -27,9 +43,26 @@ Map_7e36_1:	spriteHeader
  spritePiece $D, 0, 4, 4, $48, 0, 0, 0, 0
  spritePiece $2D, -$20, 1, 2, $58, 0, 0, 0, 0
  spritePiece $2D, 8, 1, 2, $5A, 0, 0, 0, 0
-Map_7e36_1_End
+Map_a7be_2_End
 
-Map_7e36_2:	spriteHeader
+Map_a7be_3:	spriteHeader
+ spritePiece $2D, 8, 4, 4, $72, 0, 0, 0, 0
+ spritePiece $4D, $10, 2, 3, $82, 0, 0, 0, 0
+ spritePiece $25, $10, 1, 3, $88, 0, 0, 0, 0
+ spritePiece -$32, -$19, 2, 1, $5C, 0, 0, 0, 0
+ spritePiece -$42, -$11, 4, 4, $5E, 0, 0, 0, 0
+ spritePiece -$22, 7, 2, 2, $6E, 0, 0, 0, 0
+ spritePiece -$23, -$20, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -3, -$20, 4, 4, $10, 0, 0, 0, 0
+ spritePiece $1D, -$20, 2, 4, $20, 0, 0, 0, 0
+ spritePiece -$33, 0, 4, 4, $28, 0, 0, 0, 0
+ spritePiece -$13, 0, 4, 4, $38, 0, 0, 0, 0
+ spritePiece $D, 0, 4, 4, $48, 0, 0, 0, 0
+ spritePiece $2D, -$20, 1, 2, $58, 0, 0, 0, 0
+ spritePiece $2D, 8, 1, 2, $5A, 0, 0, 0, 0
+Map_a7be_3_End
+
+Map_a7be_4:	spriteHeader
  spritePiece $39, -8, 4, 4, $72, 0, 0, 0, 0
  spritePiece $59, 0, 2, 3, $82, 0, 0, 0, 0
  spritePiece $29, 8, 2, 3, $88, 0, 0, 0, 0
@@ -45,15 +78,15 @@ Map_7e36_2:	spriteHeader
  spritePiece $D, 0, 4, 4, $48, 0, 0, 0, 0
  spritePiece $2D, -$20, 1, 2, $58, 0, 0, 0, 0
  spritePiece $2D, 8, 1, 2, $5A, 0, 0, 0, 0
-Map_7e36_2_End
+Map_a7be_4_End
 
-Map_7e36_3:	spriteHeader
+Map_a7be_5:	spriteHeader
  spritePiece $39, -8, 4, 4, $72, 0, 0, 0, 0
  spritePiece $59, 0, 2, 3, $82, 0, 0, 0, 0
  spritePiece $29, 8, 2, 3, $88, 0, 0, 0, 0
  spritePiece $39, $18, 4, 1, $8E, 0, 0, 0, 0
- spritePiece -$32, -$19, 2, 1, $5C, 0, 0, 0, 0
- spritePiece -$42, -$11, 4, 4, $5E, 0, 0, 0, 0
+ spritePiece -$32, -$19, 2, 1, $92, 0, 0, 0, 0
+ spritePiece -$42, -$11, 4, 4, $94, 0, 0, 0, 0
  spritePiece -$22, 7, 2, 2, $6E, 0, 0, 0, 0
  spritePiece -$23, -$20, 4, 4, 0, 0, 0, 0, 0
  spritePiece -3, -$20, 4, 4, $10, 0, 0, 0, 0
@@ -63,6 +96,6 @@ Map_7e36_3:	spriteHeader
  spritePiece $D, 0, 4, 4, $48, 0, 0, 0, 0
  spritePiece $2D, -$20, 1, 2, $58, 0, 0, 0, 0
  spritePiece $2D, 8, 1, 2, $5A, 0, 0, 0, 0
-Map_7e36_3_End
+Map_a7be_5_End
 
 	even
