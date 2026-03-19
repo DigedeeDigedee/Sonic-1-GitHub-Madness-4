f06ec57d6fdf06256e69b2c582416a6691f3db05
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-03-19 14:49:16 -0500

    backup

---
+++ _maps/Shield Map.asm
@@ -0,0 +1,51 @@
+Map_fe7d: mappingsTable
+	mappingsTableEntry.w	Map_fe7d_0
+	mappingsTableEntry.w	Map_fe7d_1
+	mappingsTableEntry.w	Map_fe7d_2
+	mappingsTableEntry.w	Map_fe7d_3
+	mappingsTableEntry.w	Map_fe7d_4
+	mappingsTableEntry.w	Map_fe7d_5
+
+Map_fe7d_0:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, 0, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, 0, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, 0, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, 0, 1, 1, 0, 0
+Map_fe7d_0_End
+
+Map_fe7d_1:	spriteHeader
+ spritePiece -$18, -$18, 4, 3, 0, 0, 0, 0, 0
+ spritePiece 8, -$18, 2, 3, $C, 0, 0, 0, 0
+ spritePiece -$18, 0, 4, 3, 0, 0, 1, 0, 0
+ spritePiece 8, 0, 2, 3, $C, 0, 1, 0, 0
+Map_fe7d_1_End
+
+Map_fe7d_2:	spriteHeader
+ spritePiece -$18, -$18, 4, 3, 0, 0, 0, 0, 0
+ spritePiece 8, -$18, 2, 3, $C, 0, 0, 0, 0
+ spritePiece -$18, 0, 4, 3, 0, 0, 1, 0, 0
+ spritePiece 8, 0, 2, 3, $C, 0, 1, 0, 0
+Map_fe7d_2_End
+
+Map_fe7d_3:	spriteHeader
+ spritePiece -$18, -$18, 3, 3, 0, 0, 0, 0, 0
+ spritePiece 0, -$18, 3, 3, 0, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 3, 0, 0, 1, 0, 0
+ spritePiece 0, 0, 3, 3, 0, 1, 1, 0, 0
+Map_fe7d_3_End
+
+Map_fe7d_4:	spriteHeader
+ spritePiece -$18, -$18, 4, 3, 0, 0, 0, 0, 0
+ spritePiece 8, -$18, 2, 3, $C, 0, 0, 0, 0
+ spritePiece -$18, 0, 4, 3, 0, 0, 1, 0, 0
+ spritePiece 8, 0, 2, 3, $C, 0, 1, 0, 0
+Map_fe7d_4_End
+
+Map_fe7d_5:	spriteHeader
+ spritePiece -$18, -$18, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$18, 0, 4, 3, 0, 0, 1, 0, 0
+ spritePiece 8, -$18, 2, 3, $C, 0, 0, 0, 0
+ spritePiece 8, 0, 2, 3, $C, 0, 1, 0, 0
+Map_fe7d_5_End
+
+	even
