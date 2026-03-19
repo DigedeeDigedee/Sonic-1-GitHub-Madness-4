a3d3fbac9cc19dcc57ea53016d63aa8d82428d14
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2026-03-19 16:56:23 -0300

    Bart (with vram errors)

--- _maps/Yadrin.asm
+++ _maps/Yadrin.asm
@@ -2,47 +2,23 @@
 ; Sprite mappings - output from SonMapEd - Sonic 1 format
 ; --------------------------------------------------------------------------------
 
-SME_k2Tod:	
-		dc.w SME_k2Tod_E-SME_k2Tod, SME_k2Tod_28-SME_k2Tod	
-		dc.w SME_k2Tod_47-SME_k2Tod, SME_k2Tod_61-SME_k2Tod	
-		dc.w SME_k2Tod_7B-SME_k2Tod, SME_k2Tod_95-SME_k2Tod	
-		dc.w SME_k2Tod_AF-SME_k2Tod	
-SME_k2Tod_E:	dc.b 5	
-		dc.b $C5, $F, 0, 0, $ED	
-		dc.b $E5, $F, 0, $10, $ED	
-		dc.b $C5, 7, 0, $20, $D	
-		dc.b $E5, 7, 0, $28, $D	
-		dc.b 5, $E, 0, $30, $ED	
-SME_k2Tod_28:	dc.b 6	
-		dc.b $C2, $F, 0, 0, $ED	
-		dc.b $E6, $F, 0, $10, $ED	
-		dc.b $C4, 7, 0, $20, $D	
-		dc.b $E4, 7, 0, $28, $D	
-		dc.b 6, $E, 0, $30, $ED	
-		dc.b $E1, $F, 0, $10, $ED	
-SME_k2Tod_47:	dc.b 5	
-		dc.b $BF, $F, 0, 0, $ED	
-		dc.b $DD, $F, 0, $10, $ED	
-		dc.b $BC, 7, 0, $20, $D	
-		dc.b $DC, 7, 0, $28, $B	
-		dc.b $FA, $E, 0, $30, $ED	
-SME_k2Tod_61:	dc.b 5	
-		dc.b $BE, $F, 0, 0, $ED	
-		dc.b $D9, $F, 0, $10, $ED	
-		dc.b $B9, 7, 0, $20, $D	
-		dc.b $D9, 7, 0, $28, $A	
-		dc.b $F6, $E, 0, $30, $ED	
-SME_k2Tod_7B:	dc.b 5	
-		dc.b $BE, $F, 0, 0, $ED	
-		dc.b $D7, $F, 0, $10, $ED	
-		dc.b $B7, 7, 0, $20, $D	
-		dc.b $D6, 7, 0, $28, 7	
-		dc.b $F0, $E, 0, $30, $ED	
-SME_k2Tod_95:	dc.b 5	
-		dc.b $BF, $F, 0, 0, $EC	
-		dc.b $D7, $F, 0, $10, $EB	
-		dc.b $B8, 7, 0, $20, $B	
-		dc.b $D8, 7, 0, $28, 7	
-		dc.b $F4, $E, 0, $30, $EB	
-SME_k2Tod_AF:	dc.b 0	
+SME_SvnnK:	
+		dc.w SME_SvnnK_8-SME_SvnnK, SME_SvnnK_22-SME_SvnnK	
+		dc.w SME_SvnnK_2D-SME_SvnnK, SME_SvnnK_47-SME_SvnnK	
+SME_SvnnK_8:	dc.b 5	
+		dc.b $CC, $F, 0, 0, $EC	
+		dc.b $EC, $E, 0, $10, $EC	
+		dc.b $CC, 7, 0, $1C, $C	
+		dc.b $EC, $B, 0, $24, $C	
+		dc.b 4, 0, 0, $30, $24	
+SME_SvnnK_22:	dc.b 2	
+		dc.b $CC, $F, 0, $31, $FC	
+		dc.b $EC, $F, 0, $41, $FC	
+SME_SvnnK_2D:	dc.b 5	
+		dc.b $CC, $F, 0, $51, $EC	
+		dc.b $EC, $F, 0, $61, $EC	
+		dc.b $CC, $B, 0, $71, $C	
+		dc.b $EC, $B, 0, $7D, $C	
+		dc.b $EC, 1, 0, $89, $24	
+SME_SvnnK_47:	dc.b 0	
 		even
