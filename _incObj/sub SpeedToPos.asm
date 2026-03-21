b66bb3ec9cb3f0e46447aca4cf7a3e6b96a86482
Malachi <malachi.ch1695@gmail.com>
2026-03-21 17:06:57 +1100

    fix up screen rendering movement routines

--- _incObj/sub SpeedToPos.asm
+++ _incObj/sub SpeedToPos.asm
@@ -6,18 +6,22 @@
 
 
 SpeedToPos:
-		move.l	obX(a0),d2
-		move.l	obY(a0),d3
-		move.w	obVelX(a0),d0	; load horizontal speed
-		ext.l	d0
-		asl.l	#8,d0		; multiply speed by $100
-		add.l	d0,d2		; add to x-axis position
-		move.w	obVelY(a0),d0	; load vertical speed
-		ext.l	d0
-		asl.l	#8,d0		; multiply by $100
-		add.l	d0,d3		; add to y-axis position
-		move.l	d2,obX(a0)	; update x-axis position
-		move.l	d3,obY(a0)	; update y-axis position
+		movem.w	obVelX(a0),d2-d3
+		asl.l	#8, d2
+		add.l	d2, obX(a0)
+		asl.l	#8, d3
+		add.l	d3, obY(a0)
 		rts
 
 ; End of function SpeedToPos
+
+; FUCK
+ScreenSpeedToPos:
+		move.w	obScreenY(a0),d0	; swap obScreenY and obY
+		move.w	obY(a0),obScreenY(a0)
+		move.w	d0,obY(a0)
+		bsr.s	SpeedToPos
+		move.w	obScreenY(a0),d0	; swap obScreenY and obY, again
+		move.w	obY(a0),obScreenY(a0)
+		move.w	d0,obY(a0)
+		rts
