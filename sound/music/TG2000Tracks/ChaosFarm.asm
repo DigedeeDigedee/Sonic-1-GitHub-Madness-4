3872b92d2a6ac5687bd9ea1bc10fd7acfc7e997c
TheGamer2000 <archivethegamer@gmail.com>
2026-03-21 15:16:09 -0400

    ChaosFarm Partially Fixed

--- sound/music/TG2000Tracks/ChaosFarm.asm
+++ sound/music/TG2000Tracks/ChaosFarm.asm
@@ -1,323 +1,206 @@
 ChaosFarm_Header:
-	smpsHeaderStartSong 1
-	smpsHeaderVoice	ChaosFarm_Voices
-	smpsHeaderChan	$06,	$03
-	smpsHeaderTempo	$01,	$00
-
+	smpsHeaderStartSong 3
+	smpsHeaderVoice		ChaosFarm_Voices
+	smpsHeaderChan		$06, $02
+	smpsHeaderTempo		$02, $45
+;	Given Tempo = 150.00 BPM
+;	Approximated Tempo = 150.29 BPM
 
 	smpsHeaderDAC	ChaosFarm_DAC
-	smpsHeaderFM	ChaosFarm_FM1,	smpsPitch00,	$00
-	smpsHeaderFM	ChaosFarm_FM2,	smpsPitch00,	$00
-	smpsHeaderFM	ChaosFarm_FM3,	smpsPitch00,	$00
-	smpsHeaderFM	ChaosFarm_FM4,	smpsPitch00,	$00
-	smpsHeaderFM	ChaosFarm_FM5,	smpsPitch00,	$00
-	smpsHeaderFM	ChaosFarm_FM6,	smpsPitch00,	$00
-	smpsHeaderPSG       ChaosFarm_PSG1,	$00, $00, $00, $00
-	smpsHeaderPSG       ChaosFarm_PSG2,	$00, $00, $00, $00
-	smpsHeaderPSG       ChaosFarm_PSG3,	$00, $00, $00, $00
-	dc.b		$00,	$00,	$00,	$00
-
-; PSG3 Data
-ChaosFarm_PSG3:
-	smpsStop
-
-; DAC Data
-ChaosFarm_DAC:
-	smpsStop
-
-
-; FM1 Data
-ChaosFarm_FM1:
-	smpsFMvoice	$00
-	smpsAlterVol	$07
-	smpsPan		panCentre,	$00
-	dc.b		nA3,	$0F,	nCs4,	nE4,	$0A,	nAb4,	$14,	nA4
-	dc.b		nD5,	$0F,	nCs5,	nB4,	$0A,	nCs5,	$28,	nRst
-	dc.b		$7F,	$21
-	smpsStop
-
-; FM2 Data
-ChaosFarm_FM2:
-	smpsFMvoice	$01
-	smpsAlterVol	$11
-	smpsPan		panRight,	$00
-	dc.b		nCs5,	$0F,	nCs5,	nCs5,	$0A,	nE5,	$14,	nCs5
-	dc.b		nB4,	$0F,	nB4,	nB4,	$0A,	nA4,	$14,	nRst
-	dc.b		$7F,	$35
-	smpsStop
-
-; FM3 Data
-ChaosFarm_FM3:
-	smpsFMvoice	$00
-	smpsAlterVol	$07
-	smpsPan		panCentre,	$00
-	dc.b		nE3,	$0F,	nAb3,	nB3,	$0A,	nEb4,	$14,	nE4
-	dc.b		nA4,	$0F,	nAb4,	nFs4,	$0A,	nA4,	$28,	nRst
-	dc.b		$7F,	$21
-	smpsStop
-
-; FM4 Data
-ChaosFarm_FM4:
-	smpsFMvoice	$00
-	smpsAlterVol	$07
-	smpsPan		panCentre,	$00
-	dc.b		nCs3,	$0F,	nE3,	nAb3,	$0A,	nB3,	$14,	nCs4
-	dc.b		nFs4,	$0F,	nE4,	nEb4,	$0A,	nE4,	$28,	nRst
-	dc.b		$7F,	$21
-	smpsStop
-
-; FM5 Data
-ChaosFarm_FM5:
-	smpsFMvoice	$01
-	smpsAlterVol	$11
-	smpsPan		panCentre,	$00
-	dc.b		nE5,	$0F,	nE5,	nE5,	$0A,	nAb5,	$14,	nE5
-	dc.b		nD5,	$0F,	nD5,	nD5,	$0A,	nCs5,	$14,	nRst
-	dc.b		$7F,	$35
-	smpsStop
-
-; FM6 Data
-ChaosFarm_FM6:
-	smpsFMvoice	$01
-	smpsAlterVol	$11
-	smpsPan		panLeft,	$00
-	dc.b		nA4,	$0F,	nA4,	nA4,	$0A,	nCs5,	$14,	nA4
-	dc.b		nAb4,	$0F,	nAb4,	nAb4,	$0A,	nE4,	$14,	nRst
-	dc.b		$7F,	$35
-	smpsStop
-
-; PSG1 Data
-ChaosFarm_PSG1:
-	dc.b		nRst,	$78,	nA4,	$08
-	smpsAlterNote	$FF
-	dc.b		nE4,	$07,	nCs4,	$08
-	smpsAlterNote	$00
-	dc.b		nA3,	$07
-	smpsPSGAlterVol	$05
-	dc.b		nA4,	$08
-	smpsAlterNote	$FF
-	dc.b		nE4,	$07,	nCs4,	$08
-	smpsAlterNote	$00
-	dc.b		nA3,	$07
-	smpsPSGAlterVol	$04
-	dc.b		nA4,	$08
-	smpsAlterNote	$FF
-	dc.b		nE4,	$07,	nCs4,	$08
-	smpsAlterNote	$00
-	dc.b		nA3,	$07
-	smpsPSGAlterVol	$03
-	dc.b		nA4,	$08
-	smpsAlterNote	$FF
-	dc.b		nE4,	$07,	nCs4,	$08
-	smpsAlterNote	$00
-	dc.b		nA3,	$05,	nRst,	$52
-	smpsStop
-
-; PSG2 Data
-ChaosFarm_PSG2:
-	dc.b		nRst,	$7B,	nA4,	$01
-	smpsPSGAlterVol	$03
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$F8
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$03
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$F7
-	dc.b		nCs4
-	smpsPSGAlterVol	$03
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$F8
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$03
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$FC
-	dc.b		nA4
-	smpsPSGAlterVol	$03
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$F8
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$03
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$F7
-	dc.b		nCs4
-	smpsPSGAlterVol	$03
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$F8
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$03
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$FB
-	dc.b		nA4
-	smpsPSGAlterVol	$03
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4
-	smpsPSGAlterVol	$01
-	dc.b		nA4,	nRst,	$03
-	smpsPSGAlterVol	$FB
-	smpsAlterNote	$FF
-	dc.b		nE4,	$01
-	smpsPSGAlterVol	$03
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$FF
-	dc.b		nE4,	nRst,	$04
-	smpsPSGAlterVol	$FB
-	dc.b		nCs4,	$01
-	smpsPSGAlterVol	$03
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4
-	smpsPSGAlterVol	$01
-	dc.b		nCs4,	nRst,	$03
-	smpsPSGAlterVol	$FB
-	smpsAlterNote	$00
-	dc.b		nA3,	$01
-	smpsPSGAlterVol	$03
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3
-	smpsPSGAlterVol	$01
-	smpsAlterNote	$00
-	dc.b		nA3,	nRst,	$04
-	smpsPSGAlterVol	$FE
-	dc.b		nA4,	$01,	nRst,	$06
-	smpsAlterNote	$FF
-	dc.b		nE4,	$01,	nRst,	$07,	nCs4,	$01,	nRst,	$06
-	smpsAlterNote	$00
-	dc.b		nA3,	$01,	nRst,	$54
-	smpsStop
+	smpsHeaderFM	ChaosFarm_FM1,	$00, $00
+	smpsHeaderFM	ChaosFarm_FM2,	$00, $00
+	smpsHeaderFM	ChaosFarm_FM3,	$00, $00
+	smpsHeaderFM	ChaosFarm_FM4,	$00, $00
+	smpsHeaderFM	ChaosFarm_FM5,	$00, $00
+	smpsHeaderPSG	ChaosFarm_PSG1,	$0C, $00, $00, $00
+	smpsHeaderPSG	ChaosFarm_PSG2,	$0C, $00, $00, $00
 
 ChaosFarm_Voices:
-	dc.b		$04,$35,$72,$54,$06,$1F,$1F,$1F,$1F,$07,$0A,$07,$0D,$00,$0B,$00
-	dc.b		$0B,$1F,$0F,$1F,$0F,$23,$15,$1D,$00;			Voice 00
-	dc.b		$3C,$31,$52,$50,$30,$52,$53,$52,$53,$08,$00,$08,$00,$04,$00,$04
-	dc.b		$00,$10,$07,$10,$07,$1A,$00,$16,$00;			Voice 01
-	smpsFooterEndSong	"TG2000Tracks/ChaosFarm.asm"
-	even
+;	FM Voice 00 -> 00: 16_chaos_emerald_41
+	smpsVcAlgorithm		$04
+	smpsVcFeedback		$00
+	smpsVcDetune		$00, $05, $07, $03
+	smpsVcCoarseFreq	$06, $04, $02, $05
+	smpsVcRateScale		$00, $00, $00, $00
+	smpsVcAttackRate	$1F, $1F, $1F, $1F
+	smpsVcAmpMod		$00, $00, $00, $00
+	smpsVcDecayRate1	$0D, $07, $0A, $07
+	smpsVcDecayRate2	$0B, $00, $0B, $00
+	smpsVcDecayLevel	$00, $01, $00, $01
+	smpsVcReleaseRate	$0F, $0F, $0F, $0F
+	smpsVcTotalLevel	$07, $1D, $1B, $23
+
+;	FM Voice 01 -> 01: 04_spring_yard_zone_42
+	smpsVcAlgorithm		$04
+	smpsVcFeedback		$07
+	smpsVcDetune		$03, $05, $05, $03
+	smpsVcCoarseFreq	$00, $00, $02, $01
+	smpsVcRateScale		$01, $01, $01, $01
+	smpsVcAttackRate	$13, $12, $13, $12
+	smpsVcAmpMod		$00, $00, $00, $00
+	smpsVcDecayRate1	$00, $08, $00, $08
+	smpsVcDecayRate2	$00, $04, $00, $04
+	smpsVcDecayLevel	$00, $01, $00, $01
+	smpsVcReleaseRate	$07, $00, $07, $00
+	smpsVcTotalLevel	$11, $16, $11, $1A
+
+;	PSG Voice 02 -> uptone_01
+;	macros:
+;		vol: 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15 15
+
+;	PSG Voice 03 -> uptone_02
+;	macros:
+;		vol: 15 12 11 10 9 8 7 6 6 5 4 4 3 3 2 2 2 2 1 1 1 1 1 1 0 0 0 0 0 0 0 0
+
+	; Loop Pattern :  00
+	; End Pattern :  02
+	; End Place :  40
+
+
+ChaosFarm_FM1:
+
+ChaosFarm_FM1_Jump:
+	smpsCall ChaosFarm_FM1_00_0_64
+	smpsCall ChaosFarm_FM1_01_0_64
+	smpsJump ChaosFarm_FM1_Jump
+
+ChaosFarm_FM1_00_0_64:
+	smpsSetvoice	$00
+	dc.b nA3, $06, nCs4, nE4, $04, nAb4, $08, nA4, nD5, $06, nCs5, nB4, $04, nCs5, $10
+	smpsStop
+
+ChaosFarm_FM1_01_0_64:
+	dc.b nRst, $08, $38
+	smpsStop
+
+ChaosFarm_FM2:
+
+ChaosFarm_FM2_Jump:
+	smpsCall ChaosFarm_FM2_00_0_64
+	smpsCall ChaosFarm_FM2_01_0_64
+	smpsJump ChaosFarm_FM2_Jump
+
+ChaosFarm_FM2_00_0_64:
+	smpsSetvoice	$01
+	smpsPan		panRight, $00
+	dc.b nCs5, $06, $06, $04, nE5, $08, nCs5, nB4, $06, $06, $04, nA4, $08, nRst
+	smpsStop
+
+ChaosFarm_FM2_01_0_64:
+	dc.b smpsNoAttack, $08, nRst, $38
+	smpsStop
+
+ChaosFarm_FM3:
+
+ChaosFarm_FM3_Jump:
+	smpsCall ChaosFarm_FM3_00_0_64
+	smpsCall ChaosFarm_FM3_01_0_64
+	smpsJump ChaosFarm_FM3_Jump
+
+ChaosFarm_FM3_00_0_64:
+	smpsSetvoice	$00
+	dc.b nE3, $06, nAb3, nB3, $04, nDs4, $08, nE4, nA4, $06, nAb4, nFs4, $04, nA4, $10
+	smpsStop
+
+ChaosFarm_FM3_01_0_64:
+	dc.b nRst, $40
+	smpsStop
+
+ChaosFarm_FM4:
+
+ChaosFarm_FM4_Jump:
+	smpsCall ChaosFarm_FM4_00_0_64
+	smpsCall ChaosFarm_FM4_01_0_64
+	smpsJump ChaosFarm_FM4_Jump
+
+ChaosFarm_FM4_00_0_64:
+	smpsSetvoice	$00
+	dc.b nCs3, $06, nE3, nAb3, $04, nB3, $08, nCs4, nFs4, $06, nE4, nDs4, $04, nE4, $10
+	smpsStop
+
+ChaosFarm_FM4_01_0_64:
+	dc.b nRst, $40
+	smpsStop
+
+ChaosFarm_FM5:
+
+ChaosFarm_FM5_Jump:
+	smpsCall ChaosFarm_FM5_00_0_64
+	smpsCall ChaosFarm_FM5_01_0_64
+	smpsJump ChaosFarm_FM5_Jump
+
+ChaosFarm_FM5_00_0_64:
+	smpsSetvoice	$01
+	dc.b nE5, $06, $06, $04, nAb5, $08, nE5, nD5, $06, $06, $04, nCs5, $08, nRst
+	smpsStop
+
+ChaosFarm_FM5_01_0_64:
+	dc.b smpsNoAttack, $40
+	smpsStop
+
+ChaosFarm_DAC:
+
+ChaosFarm_DAC_Jump:
+	smpsCall ChaosFarm_DAC_00_0_64
+	smpsCall ChaosFarm_DAC_01_0_64
+	smpsJump ChaosFarm_DAC_Jump
+
+ChaosFarm_DAC_00_0_64:
+	smpsSetvoice	$01
+	smpsPan		panLeft, $00
+	dc.b nA4, $06, $06, $04, nCs5, $08, nA4, nAb4, $06, $06, $04, nE4, $08, nRst
+	smpsStop
+
+ChaosFarm_DAC_01_0_64:
+	dc.b nRst, $40
+	smpsStop
+
+ChaosFarm_PSG1:
+
+ChaosFarm_PSG1_Jump:
+	smpsCall ChaosFarm_PSG1_00_0_64_0F
+	smpsCall ChaosFarm_PSG1_01_0_64_0A
+	smpsJump ChaosFarm_PSG1_Jump
+
+ChaosFarm_PSG1_00_0_64_0F:
+	dc.b smpsNoAttack, $30
+	smpsPSGvoice	uptone_01
+	dc.b nMaxPSG, $03, nE4, nCs4, nA3
+	smpsPSGAlterVol	$05
+	dc.b nMaxPSG, nE4, $01
+	smpsStop
+
+ChaosFarm_PSG1_01_0_64_0A:
+	dc.b smpsNoAttack, $02
+	smpsPSGvoice	uptone_01
+	dc.b nCs4, $03, nA3
+	smpsPSGAlterVol	$04
+	dc.b nMaxPSG, nE4, nCs4, nA3
+	smpsPSGAlterVol	$03
+	dc.b nMaxPSG, nE4, nCs4, nA3, $02, nRst, $21
+	smpsStop
+
+ChaosFarm_PSG2:
+
+ChaosFarm_PSG2_Jump:
+	smpsCall ChaosFarm_PSG2_00_0_64_0F
+	smpsCall ChaosFarm_PSG2_01_0_64_0A
+	smpsJump ChaosFarm_PSG2_Jump
+
+ChaosFarm_PSG2_00_0_64_0F:
+	dc.b smpsNoAttack, $31
+	smpsPSGvoice	uptone_02
+	dc.b nMaxPSG, $03, nE4, nCs4, nA3
+	smpsPSGAlterVol	$05
+	dc.b nMaxPSG
+	smpsStop
+
+ChaosFarm_PSG2_01_0_64_0A:
+	dc.b nE4, $03
+	smpsPSGvoice	uptone_02
+	dc.b nCs4, nA3
+	smpsPSGAlterVol	$04
+	dc.b nMaxPSG, nE4, nCs4, nA3
+	smpsPSGAlterVol	$03
+	dc.b nMaxPSG, nE4, nCs4, nA3, $02, nRst, $20
+	smpsStop
