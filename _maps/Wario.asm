1a7b60b50f17c78ba6e76e787bb570609f16c7b3
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2026-03-22 21:26:42 -0300

    Wario maps

---
+++ _maps/Wario.asm
@@ -0,0 +1,52 @@
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME__rnHN:	
+		dc.w SME__rnHN_8-SME__rnHN, SME__rnHN_3B-SME__rnHN	
+		dc.w SME__rnHN_6E-SME__rnHN, SME__rnHN_A1-SME__rnHN	
+SME__rnHN_8:	dc.b $A	
+		dc.b $E7, $F, $20, 8, $F0	
+		dc.b $E7, 3, $20, $18, $10	
+		dc.b $E7, 1, $20, $1C, $18	
+		dc.b $EE, 5, 0, $1E, $E3	
+		dc.b $EE, 5, 0, $22, $13	
+		dc.b 2, $C, 0, $34, $F0	
+		dc.b 2, 4, 0, $38, $10	
+		dc.b $DB, $E, 0, $26, $F3	
+		dc.b $DB, 1, 0, $32, $13	
+		dc.b $D4, $D, $20, 0, $F7	
+SME__rnHN_3B:	dc.b $A	
+		dc.b $E7, $F, $20, 8, $F0	
+		dc.b $E7, 3, $20, $18, $10	
+		dc.b $E7, 1, $20, $1C, $18	
+		dc.b $EE, 5, 0, $1E, $E3	
+		dc.b $EE, 5, 0, $22, $13	
+		dc.b 2, $C, 0, $34, $F0	
+		dc.b 2, 4, 0, $38, $10	
+		dc.b $DB, $E, 0, $26, $F3	
+		dc.b $DB, 1, 0, $32, $13	
+		dc.b $D4, $D, $20, 0, $F7	
+SME__rnHN_6E:	dc.b $A	
+		dc.b $E7, $F, $20, 8, $F0	
+		dc.b $E7, 3, $20, $18, $10	
+		dc.b $E7, 1, $20, $1C, $18	
+		dc.b $EE, 5, 0, $1E, $E3	
+		dc.b $EE, 5, 0, $22, $13	
+		dc.b 2, $C, 0, $34, $F0	
+		dc.b 2, 4, 0, $38, $10	
+		dc.b $DB, $E, 0, $26, $F3	
+		dc.b $DB, 1, 0, $32, $13	
+		dc.b $D4, $D, $20, 0, $F7	
+SME__rnHN_A1:	dc.b $A	
+		dc.b $E7, $F, $20, 8, $F0	
+		dc.b $E7, 3, $20, $18, $10	
+		dc.b $E7, 1, $20, $1C, $18	
+		dc.b $EE, 5, 0, $1E, $E3	
+		dc.b $EE, 5, 0, $22, $13	
+		dc.b 2, $C, 0, $34, $F0	
+		dc.b 2, 4, 0, $38, $10	
+		dc.b $DB, $E, 0, $26, $F3	
+		dc.b $DB, 1, 0, $32, $13	
+		dc.b $D4, $D, $20, 0, $F7	
+		even
