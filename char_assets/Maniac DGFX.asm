9b5dccb468693b873d2841d97ee2b1112f00c254
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-23 06:07:38 +0300

    Diagonal Manic

--- char_assets/Maniac DGFX.asm
+++ char_assets/Maniac DGFX.asm
@@ -1,112 +1,164 @@
-DPLC_e744: mappingsTable
-	mappingsTableEntry.w	DPLC_e744_0
-	mappingsTableEntry.w	DPLC_e744_1
-	mappingsTableEntry.w	DPLC_e744_2
-	mappingsTableEntry.w	DPLC_e744_3
-	mappingsTableEntry.w	DPLC_e744_4
-	mappingsTableEntry.w	DPLC_e744_5
-	mappingsTableEntry.w	DPLC_e744_6
-	mappingsTableEntry.w	DPLC_e744_7
-	mappingsTableEntry.w	DPLC_e744_8
-	mappingsTableEntry.w	DPLC_e744_9
-	mappingsTableEntry.w	DPLC_e744_10
-	mappingsTableEntry.w	DPLC_e744_11
-	mappingsTableEntry.w	DPLC_e744_12
-	mappingsTableEntry.w	DPLC_e744_13
-	mappingsTableEntry.w	DPLC_e744_14
-	mappingsTableEntry.w	DPLC_e744_15
-	mappingsTableEntry.w	DPLC_e744_16
-	mappingsTableEntry.w	DPLC_e744_17
-	mappingsTableEntry.w	DPLC_e744_18
-	mappingsTableEntry.w	DPLC_e744_19
-	mappingsTableEntry.w	DPLC_e744_20
+DPLC_4d3d: mappingsTable
+	mappingsTableEntry.w	DPLC_4d3d_0
+	mappingsTableEntry.w	DPLC_4d3d_1
+	mappingsTableEntry.w	DPLC_4d3d_2
+	mappingsTableEntry.w	DPLC_4d3d_3
+	mappingsTableEntry.w	DPLC_4d3d_4
+	mappingsTableEntry.w	DPLC_4d3d_5
+	mappingsTableEntry.w	DPLC_4d3d_6
+	mappingsTableEntry.w	DPLC_4d3d_7
+	mappingsTableEntry.w	DPLC_4d3d_8
+	mappingsTableEntry.w	DPLC_4d3d_9
+	mappingsTableEntry.w	DPLC_4d3d_10
+	mappingsTableEntry.w	DPLC_4d3d_11
+	mappingsTableEntry.w	DPLC_4d3d_12
+	mappingsTableEntry.w	DPLC_4d3d_13
+	mappingsTableEntry.w	DPLC_4d3d_14
+	mappingsTableEntry.w	DPLC_4d3d_15
+	mappingsTableEntry.w	DPLC_4d3d_16
+	mappingsTableEntry.w	DPLC_4d3d_17
+	mappingsTableEntry.w	DPLC_4d3d_18
+	mappingsTableEntry.w	DPLC_4d3d_19
+	mappingsTableEntry.w	DPLC_4d3d_20
+	mappingsTableEntry.w	DPLC_4d3d_21
+	mappingsTableEntry.w	DPLC_4d3d_22
+	mappingsTableEntry.w	DPLC_4d3d_23
+	mappingsTableEntry.w	DPLC_4d3d_24
+	mappingsTableEntry.w	DPLC_4d3d_25
+	mappingsTableEntry.w	DPLC_4d3d_26
+	mappingsTableEntry.w	DPLC_4d3d_27
+	mappingsTableEntry.w	DPLC_4d3d_28
+	mappingsTableEntry.w	DPLC_4d3d_29
 
-DPLC_e744_0:	dplcHeader
-DPLC_e744_0_End
+DPLC_4d3d_0:	dplcHeader
+DPLC_4d3d_0_End
 
-DPLC_e744_1:	dplcHeader
- dplcEntry $E, $509
-DPLC_e744_1_End
+DPLC_4d3d_1:	dplcHeader
+ dplcEntry $E, 0
+DPLC_4d3d_1_End
 
-DPLC_e744_2:	dplcHeader
- dplcEntry $E, $517
-DPLC_e744_2_End
+DPLC_4d3d_2:	dplcHeader
+ dplcEntry $E, $E
+DPLC_4d3d_2_End
 
-DPLC_e744_3:	dplcHeader
- dplcEntry $E, $525
-DPLC_e744_3_End
+DPLC_4d3d_3:	dplcHeader
+ dplcEntry $E, $1C
+DPLC_4d3d_3_End
 
-DPLC_e744_4:	dplcHeader
- dplcEntry $E, $533
-DPLC_e744_4_End
+DPLC_4d3d_4:	dplcHeader
+ dplcEntry $E, $2A
+DPLC_4d3d_4_End
 
-DPLC_e744_5:	dplcHeader
- dplcEntry $10, $571
-DPLC_e744_5_End
+DPLC_4d3d_5:	dplcHeader
+ dplcEntry $10, $38
+DPLC_4d3d_5_End
 
-DPLC_e744_6:	dplcHeader
- dplcEntry $E, $563
-DPLC_e744_6_End
+DPLC_4d3d_6:	dplcHeader
+ dplcEntry $E, $48
+DPLC_4d3d_6_End
 
-DPLC_e744_7:	dplcHeader
- dplcEntry $10, $553
-DPLC_e744_7_End
+DPLC_4d3d_7:	dplcHeader
+ dplcEntry $10, $56
+DPLC_4d3d_7_End
 
-DPLC_e744_8:	dplcHeader
- dplcEntry $10, $541
- dplcEntry 2, $551
-DPLC_e744_8_End
+DPLC_4d3d_8:	dplcHeader
+ dplcEntry $10, $66
+ dplcEntry 5, $76
+DPLC_4d3d_8_End
 
-DPLC_e744_9:	dplcHeader
- dplcEntry $10, $541
- dplcEntry 2, $551
-DPLC_e744_9_End
+DPLC_4d3d_9:	dplcHeader
+ dplcEntry $10, $7B
+ dplcEntry 4, $8B
+DPLC_4d3d_9_End
 
-DPLC_e744_10:	dplcHeader
- dplcEntry $10, $581
- dplcEntry 3, $591
-DPLC_e744_10_End
+DPLC_4d3d_10:	dplcHeader
+ dplcEntry $10, $8F
+ dplcEntry 5, $9F
+ dplcEntry 1, $139
+DPLC_4d3d_10_End
 
-DPLC_e744_11:	dplcHeader
- dplcEntry $10, $594
- dplcEntry 1, $5A4
-DPLC_e744_11_End
+DPLC_4d3d_11:	dplcHeader
+ dplcEntry $10, $13A
+DPLC_4d3d_11_End
 
-DPLC_e744_12:	dplcHeader
- dplcEntry $D, $5A5
-DPLC_e744_12_End
+DPLC_4d3d_12:	dplcHeader
+ dplcEntry $E, $14B
+DPLC_4d3d_12_End
 
-DPLC_e744_13:	dplcHeader
- dplcEntry $10, $5B6
- dplcEntry 1, $5C6
-DPLC_e744_13_End
+DPLC_4d3d_13:	dplcHeader
+ dplcEntry $10, $159
+DPLC_4d3d_13_End
 
-DPLC_e744_14:	dplcHeader
- dplcEntry $10, $5C7
-DPLC_e744_14_End
+DPLC_4d3d_14:	dplcHeader
+ dplcEntry $10, $169
+ dplcEntry 5, $179
+DPLC_4d3d_14_End
 
-DPLC_e744_15:	dplcHeader
- dplcEntry 9, $5D7
-DPLC_e744_15_End
+DPLC_4d3d_15:	dplcHeader
+ dplcEntry $10, $17E
+ dplcEntry 4, $18E
+DPLC_4d3d_15_End
 
-DPLC_e744_16:	dplcHeader
- dplcEntry 9, $5E0
-DPLC_e744_16_End
+DPLC_4d3d_16:	dplcHeader
+ dplcEntry $10, $192
+ dplcEntry 6, $1A2
+DPLC_4d3d_16_End
 
-DPLC_e744_17:	dplcHeader
- dplcEntry 9, $5E9
-DPLC_e744_17_End
+DPLC_4d3d_17:	dplcHeader
+ dplcEntry $10, $A4
+ dplcEntry 2, $B4
+DPLC_4d3d_17_End
 
-DPLC_e744_18:	dplcHeader
- dplcEntry 9, $5F2
-DPLC_e744_18_End
+DPLC_4d3d_18:	dplcHeader
+ dplcEntry $10, $A4
+ dplcEntry 2, $B4
+DPLC_4d3d_18_End
 
-DPLC_e744_19:	dplcHeader
- dplcEntry 9, $5FB
-DPLC_e744_19_End
+DPLC_4d3d_19:	dplcHeader
+ dplcEntry $10, $B6
+ dplcEntry 3, $C6
+DPLC_4d3d_19_End
 
-DPLC_e744_20:	dplcHeader
- dplcEntry 4, $5B2
-DPLC_e744_20_End
+DPLC_4d3d_20:	dplcHeader
+ dplcEntry $10, $C9
+ dplcEntry 1, $D9
+DPLC_4d3d_20_End
+
+DPLC_4d3d_21:	dplcHeader
+ dplcEntry $D, $DA
+DPLC_4d3d_21_End
+
+DPLC_4d3d_22:	dplcHeader
+ dplcEntry $10, $E7
+ dplcEntry 1, $F7
+DPLC_4d3d_22_End
+
+DPLC_4d3d_23:	dplcHeader
+ dplcEntry $10, $F8
+DPLC_4d3d_23_End
+
+DPLC_4d3d_24:	dplcHeader
+ dplcEntry 9, $108
+DPLC_4d3d_24_End
+
+DPLC_4d3d_25:	dplcHeader
+ dplcEntry 9, $111
+DPLC_4d3d_25_End
+
+DPLC_4d3d_26:	dplcHeader
+ dplcEntry 9, $11A
+DPLC_4d3d_26_End
+
+DPLC_4d3d_27:	dplcHeader
+ dplcEntry 9, $123
+DPLC_4d3d_27_End
+
+DPLC_4d3d_28:	dplcHeader
+ dplcEntry 9, $12C
+DPLC_4d3d_28_End
+
+DPLC_4d3d_29:	dplcHeader
+ dplcEntry 4, $135
+DPLC_4d3d_29_End
 
 	even
--- char_assets/Maniac Map.asm
+++ char_assets/Maniac Map.asm
@@ -1,128 +1,190 @@
-Map_22e7: mappingsTable
-	mappingsTableEntry.w	Map_22e7_0
-	mappingsTableEntry.w	Map_22e7_1
-	mappingsTableEntry.w	Map_22e7_2
-	mappingsTableEntry.w	Map_22e7_3
-	mappingsTableEntry.w	Map_22e7_4
-	mappingsTableEntry.w	Map_22e7_5
-	mappingsTableEntry.w	Map_22e7_6
-	mappingsTableEntry.w	Map_22e7_7
-	mappingsTableEntry.w	Map_22e7_8
-	mappingsTableEntry.w	Map_22e7_9
-	mappingsTableEntry.w	Map_22e7_10
-	mappingsTableEntry.w	Map_22e7_11
-	mappingsTableEntry.w	Map_22e7_12
-	mappingsTableEntry.w	Map_22e7_13
-	mappingsTableEntry.w	Map_22e7_14
-	mappingsTableEntry.w	Map_22e7_15
-	mappingsTableEntry.w	Map_22e7_16
-	mappingsTableEntry.w	Map_22e7_17
-	mappingsTableEntry.w	Map_22e7_18
-	mappingsTableEntry.w	Map_22e7_19
-	mappingsTableEntry.w	Map_22e7_20
+Map_db4d: mappingsTable
+	mappingsTableEntry.w	Map_db4d_0
+	mappingsTableEntry.w	Map_db4d_1
+	mappingsTableEntry.w	Map_db4d_2
+	mappingsTableEntry.w	Map_db4d_3
+	mappingsTableEntry.w	Map_db4d_4
+	mappingsTableEntry.w	Map_db4d_5
+	mappingsTableEntry.w	Map_db4d_6
+	mappingsTableEntry.w	Map_db4d_7
+	mappingsTableEntry.w	Map_db4d_8
+	mappingsTableEntry.w	Map_db4d_9
+	mappingsTableEntry.w	Map_db4d_10
+	mappingsTableEntry.w	Map_db4d_11
+	mappingsTableEntry.w	Map_db4d_12
+	mappingsTableEntry.w	Map_db4d_13
+	mappingsTableEntry.w	Map_db4d_14
+	mappingsTableEntry.w	Map_db4d_15
+	mappingsTableEntry.w	Map_db4d_16
+	mappingsTableEntry.w	Map_db4d_17
+	mappingsTableEntry.w	Map_db4d_18
+	mappingsTableEntry.w	Map_db4d_19
+	mappingsTableEntry.w	Map_db4d_20
+	mappingsTableEntry.w	Map_db4d_21
+	mappingsTableEntry.w	Map_db4d_22
+	mappingsTableEntry.w	Map_db4d_23
+	mappingsTableEntry.w	Map_db4d_24
+	mappingsTableEntry.w	Map_db4d_25
+	mappingsTableEntry.w	Map_db4d_26
+	mappingsTableEntry.w	Map_db4d_27
+	mappingsTableEntry.w	Map_db4d_28
+	mappingsTableEntry.w	Map_db4d_29
 
-Map_22e7_0:	spriteHeader
-Map_22e7_0_End
+Map_db4d_0:	spriteHeader
+Map_db4d_0_End
 
-Map_22e7_1:	spriteHeader
+Map_db4d_1:	spriteHeader
  spritePiece -$E, -$F, 3, 4, 0, 0, 0, 0, 0
  spritePiece -$16, -7, 1, 1, $C, 0, 0, 0, 0
  spritePiece $A, 1, 1, 1, $D, 0, 0, 0, 0
-Map_22e7_1_End
+Map_db4d_1_End
 
-Map_22e7_2:	spriteHeader
+Map_db4d_2:	spriteHeader
  spritePiece -$E, -$F, 3, 4, 0, 0, 0, 0, 0
  spritePiece $A, -7, 1, 2, $C, 0, 0, 0, 0
-Map_22e7_2_End
+Map_db4d_2_End
 
-Map_22e7_3:	spriteHeader
+Map_db4d_3:	spriteHeader
  spritePiece -$E, -$F, 3, 4, 0, 0, 0, 0, 0
  spritePiece $A, -7, 1, 2, $C, 0, 0, 0, 0
-Map_22e7_3_End
+Map_db4d_3_End
 
-Map_22e7_4:	spriteHeader
+Map_db4d_4:	spriteHeader
  spritePiece -$E, -$F, 3, 4, 0, 0, 0, 0, 0
  spritePiece $A, -7, 1, 2, $C, 0, 0, 0, 0
-Map_22e7_4_End
+Map_db4d_4_End
 
-Map_22e7_5:	spriteHeader
+Map_db4d_5:	spriteHeader
  spritePiece -$F, -$10, 3, 4, 0, 0, 0, 0, 0
  spritePiece -$F, $10, 2, 1, $C, 0, 0, 0, 0
  spritePiece 9, 0, 1, 2, $E, 0, 0, 0, 0
-Map_22e7_5_End
+Map_db4d_5_End
 
-Map_22e7_6:	spriteHeader
+Map_db4d_6:	spriteHeader
  spritePiece -$10, -$F, 3, 4, 0, 0, 0, 0, 0
  spritePiece 8, 1, 1, 2, $C, 0, 0, 0, 0
-Map_22e7_6_End
+Map_db4d_6_End
 
-Map_22e7_7:	spriteHeader
+Map_db4d_7:	spriteHeader
  spritePiece -$11, -$10, 3, 4, 0, 0, 0, 0, 0
  spritePiece -9, $10, 2, 1, $C, 0, 0, 0, 0
  spritePiece 7, 0, 2, 1, $E, 0, 0, 0, 0
-Map_22e7_7_End
+Map_db4d_7_End
 
-Map_22e7_8:	spriteHeader
+Map_db4d_8:	spriteHeader
+ spritePiece -$13, -$15, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $D, -$D, 1, 2, $10, 0, 0, 0, 0
+ spritePiece -$B, $B, 3, 1, $12, 0, 0, 0, 0
+Map_db4d_8_End
+
+Map_db4d_9:	spriteHeader
+ spritePiece -$14, -$14, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $C, -$C, 1, 3, $10, 0, 0, 0, 0
+ spritePiece 4, $C, 1, 1, $13, 0, 0, 0, 0
+Map_db4d_9_End
+
+Map_db4d_10:	spriteHeader
+ spritePiece -$15, -$15, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $B, -$15, 1, 4, $10, 0, 0, 0, 0
+ spritePiece -5, $B, 2, 1, $14, 0, 0, 0, 0
+Map_db4d_10_End
+
+Map_db4d_11:	spriteHeader
+ spritePiece -$10, -9, 4, 3, 0, 0, 0, 0, 0
+ spritePiece 0, -$11, 2, 1, $C, 0, 0, 0, 0
+ spritePiece $10, -1, 1, 2, $E, 0, 0, 0, 0
+Map_db4d_11_End
+
+Map_db4d_12:	spriteHeader
+ spritePiece -$F, -8, 4, 3, 0, 0, 0, 0, 0
+ spritePiece 1, -$10, 2, 1, $C, 0, 0, 0, 0
+Map_db4d_12_End
+
+Map_db4d_13:	spriteHeader
+ spritePiece -$10, -7, 4, 3, 0, 0, 0, 0, 0
+ spritePiece 0, -$17, 1, 2, $C, 0, 0, 0, 0
+ spritePiece $10, -7, 1, 2, $E, 0, 0, 0, 0
+Map_db4d_13_End
+
+Map_db4d_14:	spriteHeader
+ spritePiece -$D, -$15, 2, 1, 0, 0, 0, 0, 0
+ spritePiece -$15, -$D, 4, 4, 2, 0, 0, 0, 0
+ spritePiece $B, -$D, 1, 3, $12, 0, 0, 0, 0
+Map_db4d_14_End
+
+Map_db4d_15:	spriteHeader
+ spritePiece -$C, -$14, 3, 1, 0, 0, 0, 0, 0
+ spritePiece -$14, -$C, 4, 4, 3, 0, 0, 0, 0
+ spritePiece $C, -$C, 1, 1, $13, 0, 0, 0, 0
+Map_db4d_15_End
+
+Map_db4d_16:	spriteHeader
+ spritePiece -$15, -$13, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$15, $D, 4, 1, $10, 0, 0, 0, 0
+ spritePiece $B, -$B, 1, 2, $14, 0, 0, 0, 0
+Map_db4d_16_End
+
+Map_db4d_17:	spriteHeader
  spritePiece -$12, -$11, 4, 2, 0, 0, 0, 0, 0
  spritePiece -$A, -1, 4, 2, 8, 0, 0, 0, 0
  spritePiece -2, $F, 1, 1, $10, 0, 0, 0, 0
  spritePiece $E, -9, 1, 1, $11, 0, 0, 0, 0
-Map_22e7_8_End
+Map_db4d_17_End
 
-Map_22e7_9:	spriteHeader
+Map_db4d_18:	spriteHeader
  spritePiece -$13, -$11, 4, 2, 0, 0, 0, 0, 0
  spritePiece -$B, -1, 4, 2, 8, 0, 0, 0, 0
  spritePiece -3, $F, 1, 1, $10, 0, 0, 0, 0
  spritePiece $D, -9, 1, 1, $11, 0, 0, 0, 0
-Map_22e7_9_End
+Map_db4d_18_End
 
-Map_22e7_10:	spriteHeader
+Map_db4d_19:	spriteHeader
  spritePiece -$14, -$14, 4, 2, 0, 0, 0, 0, 0
  spritePiece -$C, -4, 3, 3, 8, 0, 0, 0, 0
  spritePiece $C, -$14, 1, 2, $11, 0, 0, 0, 0
-Map_22e7_10_End
+Map_db4d_19_End
 
-Map_22e7_11:	spriteHeader
+Map_db4d_20:	spriteHeader
  spritePiece -$10, -$14, 4, 4, 0, 0, 0, 0, 0
  spritePiece -$10, $C, 1, 1, $10, 0, 0, 0, 0
-Map_22e7_11_End
+Map_db4d_20_End
 
-Map_22e7_12:	spriteHeader
+Map_db4d_21:	spriteHeader
  spritePiece -$E, -$F, 3, 4, 0, 0, 0, 0, 0
  spritePiece -$16, -7, 1, 1, $C, 0, 0, 0, 0
-Map_22e7_12_End
+Map_db4d_21_End
 
-Map_22e7_13:	spriteHeader
+Map_db4d_22:	spriteHeader
  spritePiece -$10, -$10, 4, 4, 0, 0, 0, 0, 0
  spritePiece -$18, 8, 1, 1, $10, 0, 0, 0, 0
-Map_22e7_13_End
+Map_db4d_22_End
 
-Map_22e7_14:	spriteHeader
+Map_db4d_23:	spriteHeader
  spritePiece -$10, -$10, 4, 4, 0, 0, 0, 0, 0
-Map_22e7_14_End
+Map_db4d_23_End
 
-Map_22e7_15:	spriteHeader
+Map_db4d_24:	spriteHeader
  spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
-Map_22e7_15_End
+Map_db4d_24_End
 
-Map_22e7_16:	spriteHeader
+Map_db4d_25:	spriteHeader
  spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
-Map_22e7_16_End
+Map_db4d_25_End
 
-Map_22e7_17:	spriteHeader
+Map_db4d_26:	spriteHeader
  spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
-Map_22e7_17_End
+Map_db4d_26_End
 
-Map_22e7_18:	spriteHeader
+Map_db4d_27:	spriteHeader
  spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
-Map_22e7_18_End
+Map_db4d_27_End
 
-Map_22e7_19:	spriteHeader
+Map_db4d_28:	spriteHeader
  spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
-Map_22e7_19_End
+Map_db4d_28_End
 
-Map_22e7_20:	spriteHeader
+Map_db4d_29:	spriteHeader
  spritePiece -$C, -2, 4, 1, 0, 0, 0, 0, 0
-Map_22e7_20_End
+Map_db4d_29_End
 
 	even
