6bcf75fd879fd332a9863e313eeaecac31e2e447
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-26 15:37:22 +0300

    WIP jp50hz logo in screensaver

---
+++ GMZ/A50Map.asm
@@ -0,0 +1,31 @@
+Map_cc88: mappingsTable
+	mappingsTableEntry.w	Map_cc88_0
+	mappingsTableEntry.w	Map_cc88_1
+	mappingsTableEntry.w	Map_cc88_2
+
+Map_cc88_0:	spriteHeader
+ spritePiece -$40, -$10, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$20, -$10, 4, 4, $10, 0, 0, 0, 0
+ spritePiece 0, -$10, 4, 4, $20, 0, 0, 0, 0
+ spritePiece $20, -$10, 4, 4, $30, 0, 0, 0, 0
+ spritePiece -$40, -$10, 4, 4, $40, 0, 0, 1, 0
+ spritePiece -$20, -$10, 4, 4, $50, 0, 0, 1, 0
+ spritePiece 0, -$10, 4, 4, $60, 0, 0, 1, 0
+ spritePiece $20, -$10, 4, 4, $70, 0, 0, 1, 0
+Map_cc88_0_End
+
+Map_cc88_1:	spriteHeader
+ spritePiece -$40, -$10, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$20, -$10, 4, 4, $10, 0, 0, 0, 0
+ spritePiece 0, -$10, 4, 4, $20, 0, 0, 0, 0
+ spritePiece $20, -$10, 4, 4, $30, 0, 0, 0, 0
+Map_cc88_1_End
+
+Map_cc88_2:	spriteHeader
+ spritePiece -$40, -$10, 4, 4, $40, 0, 0, 1, 0
+ spritePiece -$20, -$10, 4, 4, $50, 0, 0, 1, 0
+ spritePiece 0, -$10, 4, 4, $60, 0, 0, 1, 0
+ spritePiece $20, -$10, 4, 4, $70, 0, 0, 1, 0
+Map_cc88_2_End
+
+	even
