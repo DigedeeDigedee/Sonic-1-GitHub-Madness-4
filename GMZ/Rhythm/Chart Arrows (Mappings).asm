6b876e3e3a32e123bb400f79bc12291284be0d0d
MaybeNotMonitor <86415343+MaybeNotMonitor@users.noreply.github.com>
2026-03-27 12:29:50 -0300

    something unfolds

---
+++ GMZ/Rhythm/Chart Arrows (Mappings).asm
@@ -0,0 +1,16 @@
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_F5qVa:	
+		dc.w SME_F5qVa_8-SME_F5qVa, SME_F5qVa_E-SME_F5qVa	
+		dc.w SME_F5qVa_14-SME_F5qVa, SME_F5qVa_1A-SME_F5qVa	
+SME_F5qVa_8:	dc.b 1	
+		dc.b $E0, $F, 0, 0, $F8	
+SME_F5qVa_E:	dc.b 1	
+		dc.b $E0, $F, 0, $10, $F8	
+SME_F5qVa_14:	dc.b 1	
+		dc.b $E0, $F, $30, $10, $F8	
+SME_F5qVa_1A:	dc.b 1	
+		dc.b $E0, $F, $28, 0, $F8	
+		even
