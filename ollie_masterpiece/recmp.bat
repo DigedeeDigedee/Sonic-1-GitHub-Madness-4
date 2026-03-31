a500d08401c8f797737e011141a538bd95521c2f
ollie-ollie-tech-deck <ollieollietechdeck.hacks@gmail.com>
2026-03-31 09:49:52 -0400

    -_-

--- ollie_masterpiece/recmp.bat
+++
@@ -1,6 +0,0 @@
-cls
-call KENSSharp.exe -r n "gfx/npc.nem"
-call KENSSharp.exe -r n "gfx/player.nem"
-call KENSSharp.exe -r n "gfx/textbox.nem"
-call KENSSharp.exe -r n "maps/test/gfx.nem"
-REM pause
